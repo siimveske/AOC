cd844e50be4579cf65910f78a3a0315b86d5a6ff
Siim Veske <siim.veske@gmail.com>
2020-12-02 12:22:28 +0200

    solve input data

--- 2020/Day_01_Report_Repair.py
+++ 2020/Day_01_Report_Repair.py
@@ -1,19 +1,36 @@
+import os
 from typing import List
 
 def test():
     entries = [1721, 979, 366, 299, 675, 1456]
-    return solve(entries)
+    result = solve(entries)
+    assert result == 514579
 
 def solve(entries: List[int]):
-    for i in range(len(entries)):
+    numberOfEntries = len(entries)
+    for i in range(numberOfEntries):
         entry1 = entries[i]
-        for j in range(len(entries)):
+        for j in range(numberOfEntries):
             if i != j:
                 entry2 = entries[j]
                 if entry1+entry2 == 2020:
                     return entry1*entry2
     return 0
 
+def readInput(filename: str):
+    data = []
+    script_location = os.path.dirname(os.path.realpath(__file__))
+    input_file_path = os.path.join(script_location, filename)
+    
+    with open(input_file_path, "r") as f:
+        for line in f:
+            data.append(int(line))
+        f.close()
+    
+    return data
 
 if __name__ == "__main__":
-    print("solution: %s" % test())
+    input_file = "day_01_input.txt"
+    data = readInput(input_file)
+    solution = solve(data)
+    print("Solution: %s" % solution)
