2bb3ac83715c1417ef868debda5dd1edfcb4ff86
Siim Veske <siim.veske@gmail.com>
2020-12-02 12:54:10 +0200

    solution for part 2

--- 2020/Day_01_Report_Repair.py
+++ 2020/Day_01_Report_Repair.py
@@ -2,20 +2,41 @@ import os
 from typing import List
 
 def test():
+    print("---- START TEST -----")
+    
     entries = [1721, 979, 366, 299, 675, 1456]
     result = solve(entries)
-    assert result == 514579
+    assert result == 241861950
+    
+    print("---- END TEST -----\n")
 
+def main():
+    print("---- PROGRAM ----")
+    
+    input_file = "day_01_input.txt"
+    data = readInput(input_file)
+    solution = solve(data)
+    
+    print("---- END PROGRAM ----\n")
+    print("Solution: %s" % solution)
+        
 def solve(entries: List[int]):
+    solution = 0
     numberOfEntries = len(entries)
     for i in range(numberOfEntries):
         entry1 = entries[i]
         for j in range(numberOfEntries):
             if i != j:
                 entry2 = entries[j]
-                if entry1+entry2 == 2020:
-                    return entry1*entry2
-    return 0
+                for k in range(numberOfEntries):
+                    if i != k and j !=k:
+                        entry3 = entries[k]
+                        if entry1+entry2+entry3 == 2020:
+                            solution = entry1*entry2*entry3
+                            print("%s + %s + %s == 2020" % (entry1, entry2, entry3))
+                            print("%s * %s * %s == %s" % (entry1, entry2, entry3, solution))
+                            return solution
+    return solution
 
 def readInput(filename: str):
     data = []
@@ -30,7 +51,5 @@ def readInput(filename: str):
     return data
 
 if __name__ == "__main__":
-    input_file = "day_01_input.txt"
-    data = readInput(input_file)
-    solution = solve(data)
-    print("Solution: %s" % solution)
+    test()
+    main()
