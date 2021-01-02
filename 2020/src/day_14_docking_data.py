c9fb02af0f92e0e1f3c3428b7a9c6585feb4a560
Siim Veske <siim.veske@gmail.com>
2021-01-02 20:05:29 +0200

    solve day 14 part 2

--- 2020/src/day_14_docking_data.py
+++ 2020/src/day_14_docking_data.py
@@ -11,6 +11,25 @@ def load_program(file):
     return program
 
 
+def apply_mask(mask, address):
+    result = []
+    mask_length = len(mask)
+    bin_address = format(address, f'0{mask_length}b')
+    for i in range(mask_length):
+        mask_bit = mask[i]
+        if mask_bit == '0':
+            # If the bitmask bit is 0, the corresponding memory address bit is unchanged
+            result.append(bin_address[i])
+        elif mask_bit == '1':
+            # If the bitmask bit is 1, the corresponding memory address bit is overwritten with 1
+            result.append('1')
+        elif mask_bit == 'X':
+            # If the bitmask bit is X, the corresponding memory address bit is floating
+            result.append('{}')
+
+    return ''.join(result)
+
+
 def part1(file):
     program = load_program(file)
 
@@ -18,8 +37,7 @@ def part1(file):
     and_mask = ''
     or_mask = ''
 
-    for line in program:
-        key, value = line
+    for key, value in program:
         if key == 'mask':
             and_mask = int(value.replace('X', '1'), 2)
             or_mask = int(value.replace('X', '0'), 2)
@@ -33,16 +51,43 @@ def part1(file):
     return sum_of_all_values
 
 
+def part2(file):
+    program = load_program(file)
+
+    memory = {}
+    mask = ''
+    floating_bit_count = 0
+    for key, value in program:
+        if key == 'mask':
+            mask = value
+            floating_bit_count = mask.count('X')
+        else:
+            address = int(key.replace('mem[', '').replace(']', ''))
+            template = apply_mask(mask, address)
+            for i in range(pow(2, floating_bit_count)):
+                b = format(i, f'0{floating_bit_count}b')
+                masked_address = template.format(*b)
+                memory[int(masked_address, 2)] = int(value)
+
+    sum_of_all_values = sum(memory.values())
+    print('part 2:', sum_of_all_values)
+    return sum_of_all_values
+
+
 def test():
     print('---- TEST ----')
     file = os.path.join('..', 'test', 'day_14_input.txt')
     assert part1(file) == 165
+
+    file = os.path.join('..', 'test', 'day_14_input2.txt')
+    assert part2(file) == 208
 
 
 def main():
     print('---- PROGRAM ----')
     file = os.path.join('..', 'data', 'day_14_input.txt')
     part1(file)
+    part2(file)
 
 
 if __name__ == '__main__':
