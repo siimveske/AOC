baae219976eb7ed7fd7ea384000fcac993d469ee
Siim Veske <siim.veske@gmail.com>
2021-01-03 02:13:32 +0200

    solve day 15 part 2

--- 2020/src/day_15_rambunctious_recitation.py
+++ 2020/src/day_15_rambunctious_recitation.py
@@ -1,10 +1,9 @@
 from collections import defaultdict
 
 
-def part1(numbers):
+def play(numbers, game_length=2020):
 
     turn = 1
-    limit = 2020
     history = defaultdict(list)
     last_number = 0
 
@@ -13,7 +12,7 @@ def part1(numbers):
         turn += 1
         last_number = number
 
-    while turn <= limit:
+    while turn <= game_length:
 
         '''first time the number had been spoken, new number spoken is 0'''
         if len(history[last_number]) == 1:
@@ -34,34 +33,49 @@ def part1(numbers):
 
         turn += 1
 
-    print('part 1:', last_number)
     return last_number
 
 
 def test():
-    print('---- TEST ----')
     numbers = [0, 3, 6]
-    assert part1(numbers) == 436
+    assert play(numbers) == 436
     numbers = [1, 3, 2]
-    assert part1(numbers) == 1
+    assert play(numbers) == 1
     numbers = [2, 1, 3]
-    assert part1(numbers) == 10
+    assert play(numbers) == 10
     numbers = [1, 2, 3]
-    assert part1(numbers) == 27
+    assert play(numbers) == 27
     numbers = [2, 3, 1]
-    assert part1(numbers) == 78
+    assert play(numbers) == 78
     numbers = [3, 2, 1]
-    assert part1(numbers) == 438
+    assert play(numbers) == 438
     numbers = [3, 1, 2]
-    assert part1(numbers) == 1836
+    assert play(numbers) == 1836
+
+    #===========================================================================
+    # numbers = [0, 3, 6]
+    # assert play(numbers, game_length=30000000) == 175594
+    # numbers = [1, 3, 2]
+    # assert play(numbers, game_length=30000000) == 2578
+    # numbers = [2, 1, 3]
+    # assert play(numbers, game_length=30000000) == 3544142
+    # numbers = [1, 2, 3]
+    # assert play(numbers, game_length=30000000) == 261214
+    # numbers = [2, 3, 1]
+    # assert play(numbers, game_length=30000000) == 6895259
+    # numbers = [3, 2, 1]
+    # assert play(numbers, game_length=30000000) == 18
+    # numbers = [3, 1, 2]
+    # assert play(numbers, game_length=30000000) == 362
+    #===========================================================================
 
 
 def main():
-    print('---- PROGRAM ----')
     numbers = [9, 12, 1, 4, 17, 0, 18]
-    part1(numbers)
+    print('part 1:', play(numbers))
+    print('part 2:', play(numbers, game_length=30000000))
 
 
 if __name__ == '__main__':
-    test()
+    # test()
     main()
