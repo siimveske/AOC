c900311f9ba116e8e25208b71ae38480897af685
Siim Veske <siim.veske@gmail.com>
2021-01-11 00:58:42 +0200

    solve day 17 part2

--- 2020/src/day_17_conway_cubes.py
+++ 2020/src/day_17_conway_cubes.py
@@ -2,7 +2,7 @@ import os
 from itertools import product
 
 
-def parse_input(file):
+def parse_input(file, N):
     script_location = os.path.dirname(os.path.realpath(__file__))
     input_file_path = os.path.join(script_location, file)
 
@@ -10,72 +10,83 @@ def parse_input(file):
         grid = [line.strip() for line in file]
 
     cube = set()
+    zeroes = [0] * (N - 2)
     for x, row in enumerate(grid):
         for y, cell in enumerate(row):
             if cell == '#':
-                cube.add((x, y, 0))
+                cube.add((x, y, *zeroes))
 
     return cube
 
 
-def neighbors(x, y, z):
-    for px in range(x - 1, x + 2):
-        for py in range(y - 1, y + 2):
-            for pz in range(z - 1, z + 2):
-                yield px, py, pz
+def neighbors(coords):
+    ranges = ((c - 1, c, c + 1) for c in coords)
+    yield from product(*ranges)
 
 
-def alive_neighbors(cube, coords):
-    alive = 0
-    for n in neighbors(*coords):
+def count_active_neighbors(cube, coords):
+    active_cnt = 0
+    for n in neighbors(coords):
         if (n in cube) and (n != coords):
-            alive += 1
+            active_cnt += 1
 
-    return alive
+    return active_cnt
 
 
 def all_neighbors(cube):
-    return set(n for p in cube for n in neighbors(*p))
+    return set(neighbor for coord in cube for neighbor in neighbors(coord))
 
 
 def evolve(cube):
     new_cube = set()
     for neighbor in all_neighbors(cube):
-        alive_cnt = alive_neighbors(cube, neighbor)
+        active_cnt = count_active_neighbors(cube, neighbor)
 
         if neighbor in cube:
-            if alive_cnt == 2 or alive_cnt == 3:
+            if active_cnt == 2 or active_cnt == 3:
                 # alive cell stays alive only if exactly 2 or 3 neighbors are alive
                 new_cube.add(neighbor)
-        elif alive_cnt == 3:
+        elif active_cnt == 3:
             # dead cell becomes alive only if exactly 3 neighbors are alive
             new_cube.add(neighbor)
 
     return new_cube
 
 
-def part1(file):
-    cube = parse_input(file)
+def solve(file, N):
+    cube = parse_input(file, N)
     for _ in range(6):
         cube = evolve(cube)
+    active_cnt = len(cube)
+    return active_cnt
 
-    total_alive = len(cube)
-    print('Part 1:', total_alive)
-    return total_alive
+
+def part1(file):
+    active_cnt = solve(file, N=3)
+    print('Part 1:', active_cnt)
+    return active_cnt
+
+
+def part2(file):
+    active_cnt = solve(file, N=4)
+    print('Part 2:', active_cnt)
+    return active_cnt
 
 
 def test():
     print('---- TEST ----')
     file = os.path.join('..', 'test', 'day_17_input.txt')
     assert part1(file) == 112
+    assert part2(file) == 848
 
 
 def main():
     print('---- PROGRAM ----')
     file = os.path.join('..', 'data', 'day_17_input.txt')
     part1(file)
+    part2(file)
 
 
 if __name__ == '__main__':
-    test()
+    #test()
     main()
