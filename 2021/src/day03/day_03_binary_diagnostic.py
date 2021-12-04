51c578408563c7a369529e9de2049e3fe750ac6b
Siim Veske <siim.veske@gmail.com>
2021-12-04 20:00:59 +0200

    solve day 3 part 1

--- 2021/src/day03/day_03_binary_diagnostic.py
+++ 2021/src/day03/day_03_binary_diagnostic.py
@@ -6,29 +6,37 @@ def readInput(filename: str):
     script_location = os.path.dirname(os.path.realpath(__file__))
     input_file_path = os.path.join(script_location, filename)
 
-    instructions = []
     with open(input_file_path, 'r') as f:
-        for line in f:
-            cmd, val = line.split(' ')
-            instructions.append((cmd, int(val)))
+        report = [line.strip() for line in f.readlines()]
 
-    return instructions
+    return report
 
 
 def part1(inputFile: str):
 
-    instructions = readInput(inputFile)
+    report = readInput(inputFile)
 
-    x, depth = 0, 0
-    for cmd, val in instructions:
-        if cmd == 'up':
-            depth -= val
-        elif cmd == 'down':
-            depth += val
-        elif cmd == 'forward':
-            x += val
+    gamma_rate = ''
+    epsilon_rate = ''
+    for col in range(len(report[0])):
+        cnt_0 = 0
+        cnt_1 = 0
+        for row in range(len(report)):
+            if report[row][col] == '0':
+                cnt_0 += 1
+            else:
+                cnt_1 += 1
+        if cnt_0 > cnt_1:
+            gamma_rate += '0'
+            epsilon_rate += '1'
+        else:
+            gamma_rate += '1'
+            epsilon_rate += '0'
 
-    return x * depth
+    gamma_rate = int(gamma_rate, 2)
+    epsilon_rate = int(epsilon_rate, 2)
+
+    return gamma_rate * epsilon_rate
 
 
 def part2(inputFile: str):
@@ -51,7 +59,7 @@ def part2(inputFile: str):
 def test():
     print('---- TEST ----')
     filename = 'test_input.txt'
-    assert part1(filename) == 150
+    assert part1(filename) == 198
     print('Part 1 OK')
     # assert part2(filename) == 900
     # print('Part 2 OK\n')
@@ -62,10 +70,10 @@ def main():
     filename = 'input.txt'
     solution_part1 = part1(filename)
     print(f'Solution for Part 1: {solution_part1}')
-    solution_part2 = part2(filename)
-    print(f'Solution for Part 2: {solution_part2}\n')
+    # solution_part2 = part2(filename)
+    # print(f'Solution for Part 2: {solution_part2}\n')
 
 
 if __name__ == '__main__':
     test()
-    #main()
+    main()
