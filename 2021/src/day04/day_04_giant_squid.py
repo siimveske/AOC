69a2467e4740a7a54f7b8e1f32c5a6686b0137fd
Siim Veske <siim.veske@gmail.com>
2021-12-06 13:26:48 +0200

    solve day 4 part 2

--- 2021/src/day04/day_04_giant_squid.py
+++ 2021/src/day04/day_04_giant_squid.py
@@ -25,34 +25,67 @@ def readInput(filename: str):
 
 
 def play_bingo(numbers, boards):
-    drawn_numbers = numbers[0:5]
 
     # Check rows
+    drawn_numbers = numbers[0:5]
     for nr in numbers[5:]:
         for board in boards:
+            # Check rows
             for row in range(5):
                 for col in range(5):
                     if board[row][col] not in drawn_numbers:
                         break
                     if col == 4:
-                        unmarked_numbers = get_unmarked_numbers(
-                            board, drawn_numbers
-                        )
+                        unmarked_numbers = get_unmarked_numbers(board, drawn_numbers)
                         return sum(unmarked_numbers) * drawn_numbers[-1]
-        drawn_numbers.append(nr)
-
-    # Check cols
-    for nr in numbers[5:]:
-        for board in boards:
+            # Check cols
             for row in range(5):
                 for col in range(5):
                     if board[col][row] not in drawn_numbers:
                         break
                     if col == 4:
-                        unmarked_numbers = get_unmarked_numbers(
-                            board, drawn_numbers
-                        )
+                        unmarked_numbers = get_unmarked_numbers(board, drawn_numbers)
                         return sum(unmarked_numbers) * drawn_numbers[-1]
+
+        drawn_numbers.append(nr)
+
+    raise Exception("Failed to solve bingo!")
+
+
+def play_bingo2(numbers, boards):
+
+    completed_boards = []
+    drawn_numbers = numbers[0:5]
+    for nr in numbers[5:]:
+        for board in boards:
+            if board in completed_boards:
+                continue
+
+            # Check rows
+            for row in range(5):
+                if board in completed_boards:
+                    break
+                for col in range(5):
+                    if board[row][col] not in drawn_numbers:
+                        break
+                    if col == 4:
+                        completed_boards.append(board)
+                        if len(completed_boards) == len(boards):
+                            unmarked_numbers = get_unmarked_numbers(board, drawn_numbers)
+                            return sum(unmarked_numbers) * drawn_numbers[-1]
+            # Check cols
+            for row in range(5):
+                if board in completed_boards:
+                    break
+                for col in range(5):
+                    if board[col][row] not in drawn_numbers:
+                        break
+                    if col == 4:
+                        completed_boards.append(board)
+                        if len(completed_boards) == len(boards):
+                            unmarked_numbers = get_unmarked_numbers(board, drawn_numbers)
+                            return sum(unmarked_numbers) * drawn_numbers[-1]
+
         drawn_numbers.append(nr)
 
     raise Exception("Failed to solve bingo!")
@@ -73,7 +106,8 @@ def part1(inputFile: str):
 
 
 def part2(inputFile: str):
-    report = readInput(inputFile)
+    numbers, boards = readInput(inputFile)
+    return play_bingo2(numbers, boards)
 
 
 def test():
@@ -81,8 +115,8 @@ def test():
     filename = 'test_input.txt'
     assert part1(filename) == 4512
     print('Part 1 OK')
-    #assert part2(filename) == 230
-    #print('Part 2 OK\n')
+    assert part2(filename) == 1924
+    print('Part 2 OK\n')
 
 
 def main():
@@ -90,8 +124,8 @@ def main():
     filename = 'input.txt'
     solution_part1 = part1(filename)
     print(f'Solution for Part 1: {solution_part1}')
-    #solution_part2 = part2(filename)
-    #print(f'Solution for Part 2: {solution_part2}\n')
+    solution_part2 = part2(filename)
+    print(f'Solution for Part 2: {solution_part2}\n')
 
 
 if __name__ == '__main__':
