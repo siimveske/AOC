00e9c0357a52348b51363803631689e34226a79b
Siim Veske <siim.veske@gmail.com>
2022-01-07 15:47:33 +0200

    solve day 11 part 1

--- 2021/src/day11/day_11_dumbo_octopus.py
+++ 2021/src/day11/day_11_dumbo_octopus.py
@@ -13,34 +13,56 @@ def readInput(filename: str):
     return data
 
 
+def get_neighbours(item, rows, cols):
+    row, col = item
+    top = (row - 1, col)
+    top_right = (row - 1, col + 1)
+    right = (row, col + 1)
+    bottom_right = (row + 1, col + 1)
+    bottom = (row + 1, col)
+    bottom_left = (row + 1, col - 1)
+    left = (row, col - 1)
+    top_left = (row - 1, col - 1)
+
+    neighbours = [top, top_right, right, bottom_right, bottom, bottom_left, left, top_left]
+    filtered_neighbours = [(i, j) for i, j in neighbours if (i >= 0 and i < rows) and (j >= 0 and j < cols)]
+
+    return filtered_neighbours
+
+
 def evolve(energy_map):
     rows = len(energy_map)
     cols = len(energy_map[0])
     flash_cnt = 0
     new_map = [[0] * cols for i in range(rows)]
 
+    flashed_octopuses = []
     for row in range(rows):
         for col in range(cols):
-            top = (row - 1, col)
-            top_right = (row - 1, col + 1)
-            right = (row, col + 1)
-            bottom_right = (row + 1, col + 1)
-            bottom = (row + 1, col)
-            bottom_left = (row + 1, col - 1)
-            left = (row, col - 1)
-            top_left = (row - 1, col - 1)
-
-            neighbours = [top, top_right, right, bottom_right, bottom, bottom_left, left, top_left]
-            filtered_neighbours = [(i, j) for i, j in neighbours if (i >= 0 and i < rows) and (j >= 0 and j < cols)]
-
-            energy_level = energy_map[row][col] + 1
-            for i, j in filtered_neighbours:
-                if (energy_map[i][j] + 1) > 9:
-                    energy_level += 1
-            if energy_level > 9:
-                energy_level = 0
+            new_value = energy_map[row][col] + 1
+            if new_value > 9:
+                flashed_octopuses.append((row, col))
+                new_map[row][col] = 0
                 flash_cnt += 1
-            new_map[row][col] = energy_level
+            else:
+                new_map[row][col] = new_value
+
+    while flashed_octopuses:
+        octopus = flashed_octopuses.pop()
+        neighbours = get_neighbours(octopus, rows, cols)
+        for row, col in neighbours:
+            new_value = new_map[row][col]
+            if new_value == 0:
+                continue
+            else:
+                new_value += 1
+
+            if new_value > 9:
+                flashed_octopuses.append((row, col))
+                new_map[row][col] = 0
+                flash_cnt += 1
+            else:
+                new_map[row][col] = new_value
 
     return (new_map, flash_cnt)
 
@@ -50,10 +72,11 @@ def part1(inputFile: str):
 
     new_map = data
     total_flash_cnt = 0
-    for i in range(10):
-        n_map, f_cnt = evolve(new_map)
-        new_map = n_map
+    for i in range(100):
+        new_map, f_cnt = evolve(new_map)
         total_flash_cnt += f_cnt
+
+    return total_flash_cnt
 
 
 def part2(inputFile):
@@ -64,10 +87,10 @@ def part2(inputFile):
 def test():
     print('---- TEST ----')
     filename = 'test_input.txt'
-    assert part1(filename) == 26397
+    assert part1(filename) == 1656
     print('Part 1 OK')
-    #assert part2(filename) == 288957
-    #print('Part 2 OK')
+    # assert part2(filename) == 288957
+    # print('Part 2 OK')
 
 
 def main():
@@ -81,4 +104,4 @@ def main():
 
 if __name__ == '__main__':
     test()
-    # main()
+    main()
