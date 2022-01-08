e5d9a8caf2b2cf7afd68d3b71d0b9a46c26f4780
Siim Veske <siim.veske@gmail.com>
2022-01-08 21:43:47 +0200

    solve day 13 part 2

--- 2021/src/day13/day_13_transparent_origami.py
+++ 2021/src/day13/day_13_transparent_origami.py
@@ -7,10 +7,8 @@ def readInput(filename: str):
     script_location = os.path.dirname(os.path.realpath(__file__))
     input_file_path = os.path.join(script_location, filename)
 
-    coordinates = set()
+    sheet = set()
     instructions = []
-    width = 0
-    height = 0
     with open(input_file_path, 'r') as f:
 
         # Parse coordinates
@@ -18,12 +16,8 @@ def readInput(filename: str):
             line = line.strip()
             if not line:
                 break
-
             x, y = line.split(',')
-            x, y = int(x), int(y)
-            width = max(x, width)
-            height = max(y, height)
-            coordinates.add((x, y))
+            sheet.add((int(x), int(y)))
 
         # Parse fold instructions
         for line in f:
@@ -31,38 +25,53 @@ def readInput(filename: str):
             key, value = line.split('=')
             instructions.append((key, int(value)))
 
-    return (coordinates, instructions, width, height)
+    return (sheet, instructions)
 
 
-def fold(coordinates, instruction):
+def fold(sheet, instruction):
     result = set()
     axis, fold_point = instruction
 
-    for x, y in coordinates:
+    for x, y in sheet:
         if axis == 'x':
-            if x < fold_point:
-                result.add((x, y))
-            else:
+            if x > fold_point:
                 delta = x - fold_point
-                result.add((fold_point - delta, y))
+                x = fold_point - delta
         else:
-            if y < fold_point:
-                result.add((x, y))
-            else:
+            if y > fold_point:
                 delta = y - fold_point
-                result.add((x, fold_point - delta))
+                y = fold_point - delta
+        result.add((x, y))
 
     return result
 
 
+def print_sheet(sheet):
+    width = 0
+    height = 0
+    for x, y in sheet:
+        width = max(width, x)
+        height = max(height, y)
+    for y in range(height + 1):
+        for x in range(width + 1):
+            if (x, y) in sheet:
+                print('#', end='')
+            else:
+                print(' ', end='')
+        print()
+
+
 def part1(inputFile: str):
-    coordinates, instructions, width, height = readInput(inputFile)
-    folded_coordinates = fold(coordinates, instructions[0])
-    return len(folded_coordinates)
+    sheet, instructions = readInput(inputFile)
+    folded_sheet = fold(sheet, instructions[0])
+    return len(folded_sheet)
 
 
 def part2(inputFile: str):
-    graph = readInput(inputFile)
+    sheet, instructions = readInput(inputFile)
+    for instruction in instructions:
+        sheet = fold(sheet, instruction)
+    return sheet
 
 
 def test():
@@ -70,8 +79,10 @@ def test():
     filename = 'test_input.txt'
     assert part1(filename) == 17
     print('Part 1 OK')
-    # assert part2(filename) == 36
-    # print('Part 2 OK')
+    solution_part2 = part2(filename)
+    assert len(solution_part2) == 16
+    print('Part 2 OK')
+    print_sheet(solution_part2)
 
 
 def main():
@@ -79,8 +90,9 @@ def main():
     filename = 'input.txt'
     solution_part1 = part1(filename)
     print(f'Solution for Part 1: {solution_part1}')
-    # solution_part2 = part2(filename)
-    # print(f'Solution for Part 2: {solution_part2}\n')
+    solution_part2 = part2(filename)
+    print(f'Solution for Part 2:')
+    print_sheet(solution_part2)
 
 
 if __name__ == '__main__':
