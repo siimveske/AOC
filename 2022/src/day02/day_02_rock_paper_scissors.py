d73adc9af598bd2f7c7c1b1e0152f59f6badc3e3
Siim Veske <siim.veske@gmail.com>
2022-12-03 12:09:04 +0200

    refactor day2

--- 2022/src/day02/day_02_rock_paper_scissors.py
+++ 2022/src/day02/day_02_rock_paper_scissors.py
@@ -6,35 +6,34 @@ def readInput(filename: str):
     script_location = os.path.dirname(os.path.realpath(__file__))
     input_file_path = os.path.join(script_location, filename)
     
-    instructions = []
     with open(input_file_path, 'r') as f:
-        for line in f:
-            a, b = line.strip().split(' ')
-            instructions.append((a, b))
+        instructions = [line.strip().split(' ') for line in f]
 
     return instructions
 
 
 # 1 for Rock, 2 for Paper, and 3 for Scissors
 shape_map = {'A':1, 'B':2, 'C':3, 'X':1, 'Y':2, 'Z':3}
+# Paper defeats Rock, Scissors defeats Paper and Rock defeats Scissors
+win_map = {'A':2, 'B':3, 'C':1}
+# Rock defeats Scissors, Paper defeats Rock and Scissors defeats Paper
+loose_map = {'A':3, 'B':1, 'C':2}
 
 
 def part1(inputFile: str):
     instructions = readInput(inputFile)
-    instructions = [(shape_map[a], shape_map[b]) for a, b in instructions]
     
     score = 0
-    win_map = {1:2, 2:3, 3:1}
-    for him, me in instructions:
+    for opponent, me in instructions:
         
         # shape you selected
-        score += me
+        score += shape_map[me]
         
         # round was a draw
-        if me == him:
+        if shape_map[me] == shape_map[opponent]:
             score += 3
         # you win
-        elif win_map[him] == me:
+        elif win_map[opponent] == shape_map[me]:
             score += 6
     
     return score
@@ -44,19 +43,16 @@ def part2(inputFile: str):
     instructions = readInput(inputFile)
     
     score = 0
-    for him, me in instructions:
+    for opponent, me in instructions:
         # round was a draw
         if me == 'Y':
-            score += (shape_map[him] + 3)
+            score += (shape_map[opponent] + 3)
         # you win
         elif me == 'Z':
-            win_map = {'A':2, 'B':3, 'C':1}
-            score += (win_map[him] + 6)
+            score += (win_map[opponent] + 6)
         # you loose
         else:
-            # Rock defeats Scissors, Paper defeats Rock and Scissors defeats Paper
-            loose_map = {'A':3, 'B':1, 'C':2}
-            score += loose_map[him]
+            score += loose_map[opponent]
             
     return score
 
