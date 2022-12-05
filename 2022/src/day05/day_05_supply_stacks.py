135241bcdae2b846cee9a1903eea3220977a822d
Siim Veske <siim.veske@gmail.com>
2022-12-05 10:08:03 +0200

    start day 5

---
+++ 2022/src/day05/day_05_supply_stacks.py
@@ -0,0 +1,44 @@
+import os
+
+
+def readInput(filename: str):
+
+    script_location = os.path.dirname(os.path.realpath(__file__))
+    input_file_path = os.path.join(script_location, filename)
+
+    sections = []
+    with open(input_file_path, 'r') as f:
+        pass
+
+    return sections
+
+
+def part1(inputFile: str) -> int:
+    sections = readInput(inputFile)
+
+
+def part2(inputFile: str) -> int:
+    sections = readInput(inputFile)
+
+
+def test():
+    print('---- TEST ----')
+    filename = 'test_input.txt'
+    assert part1(filename) == 2
+    print('Part 1 OK')
+    #assert part2(filename) == 4
+    #print('Part 2 OK\n')
+
+
+def main():
+    print('---- MAIN ----')
+    filename = 'input.txt'
+    solution_part1 = part1(filename)
+    print(f'Solution for Part 1: {solution_part1}')
+    #solution_part2 = part2(filename)
+    #print(f'Solution for Part 2: {solution_part2}\n')
+
+
+if __name__ == '__main__':
+    test()
+    main()
