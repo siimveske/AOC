d07e5cc27772f4006c7eaf2b41c6e5174b954829
Siim Veske <siim.veske@gmail.com>
2022-12-12 23:00:54 +0200

    solve day 9 part 2

--- 2022/src/day09/day_09_rope_bridge.py
+++ 2022/src/day09/day_09_rope_bridge.py
@@ -1,4 +1,7 @@
 import os
+"""
+https://github.com/mebeim/aoc/blob/master/2022/README.md#day-9---rope-bridge
+"""
 
 
 def readInput(filename: str):
@@ -14,13 +17,12 @@ def readInput(filename: str):
     return commands
 
 
+DELTA = {'U': (0, 1), 'D': (0, -1), 'L': (-1, 0), 'R': (1, 0)}
+
+
 def part1(inputFile: str) -> int:
     commands = readInput(inputFile)
-    directions = {
-        'U': (0, 1),
-        'D': (0, -1),
-        'L': (-1, 0),
-        'R': (1, 0)}
+
     visited = set([(0, 0)])
     head = (0, 0)
     tail = (0, 0)
@@ -29,7 +31,7 @@ def part1(inputFile: str) -> int:
         for i in range(val):
             head_x, head_y = head
             tail_x, tail_y = tail
-            delta_x, delta_y = directions[cmd]
+            delta_x, delta_y = DELTA[cmd]
             new_head_x = head_x + delta_x
             new_head_y = head_y + delta_y
             if abs(new_head_x - tail_x) > 1 or abs(new_head_y - tail_y) > 1:
@@ -41,7 +43,39 @@ def part1(inputFile: str) -> int:
 
 
 def part2(inputFile: str) -> int:
-    grid = readInput(inputFile)
+    commands = readInput(inputFile)
+
+    rope = [(0, 0)] * 10
+    visited = {(0, 0)}
+
+    for command in commands:
+        direction, steps = command
+        for _ in range(steps):
+            hx, hy = rope[0]
+            dx, dy = DELTA[direction]
+
+            # Move the head
+            rope[0] = hx + dx, hy + dy
+
+            for i in range(9):
+                # Consider this piece as the "head"
+                hx, hy = rope[i]
+                # And the one after it as the "tail"
+                tx, ty = rope[i + 1]
+
+                # Do the exact same thing we did for part 1
+                dx, dy = hx - tx, hy - ty
+                if dx**2 + dy**2 > 2:
+                    if dx != 0:
+                        tx += 1 if dx > 0 else -1
+                    if dy != 0:
+                        ty += 1 if dy > 0 else -1
+                    rope[i + 1] = (tx, ty)
+
+            # Keep track of tail
+            visited.add(tuple(rope[9]))
+
+    return len(visited)
 
 
 def test():
@@ -51,8 +85,8 @@ def test():
     assert part1(filename) == 13
     print('Part 1 OK')
 
-    # assert part2(filename) == 8
-    # print('Part 2 OK\n')
+    assert part2(filename) == 1
+    print('Part 2 OK\n')
 
 
 def main():
@@ -63,9 +97,9 @@ def main():
     assert solution_part1 == 5907
     print(f'Solution for Part 1: {solution_part1}')
 
-    # solution_part2 = part2(filename)
-    # assert solution_part2 == 287040
-    # print(f'Solution for Part 2: {solution_part2}\n')
+    solution_part2 = part2(filename)
+    assert solution_part2 == 2303
+    print(f'Solution for Part 2: {solution_part2}\n')
 
 
 if __name__ == '__main__':
