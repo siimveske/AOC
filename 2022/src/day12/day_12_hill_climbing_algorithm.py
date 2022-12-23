baf1bb2254e6877f25f369baeadfc87fe1c0407c
Siim Veske <siim.veske@gmail.com>
2022-12-23 22:20:33 +0200

    save progress

--- 2022/src/day12/day_12_hill_climbing_algorithm.py
+++ 2022/src/day12/day_12_hill_climbing_algorithm.py
@@ -1,6 +1,5 @@
 import os
 from collections import deque
-from math import lcm
 
 
 def readInput(filename: str):
@@ -8,15 +7,65 @@ def readInput(filename: str):
     script_location = os.path.dirname(os.path.realpath(__file__))
     input_file_path = os.path.join(script_location, filename)
 
+    grid = []
     with open(input_file_path, 'r') as f:
-        monkeys = f.read()
-        monkeys = monkeys.split('\n\n')
+        for line in f:
+            row = [character for character in line.strip()]
+            grid.append(row)
+    return grid
 
-    return parsed_monkeys
+
+def explore(grid: list, starting_row: int, starting_col: int) -> int:
+    visited = set()
+    queue = deque([(starting_row, starting_col, 0, ord('a'))])
+
+    while queue:
+        row, col, distance, height = queue.popleft()
+
+        row_inbounds = 0 <= row < len(grid)
+        col_inbounds = 0 <= col < len(grid[0])
+
+        if (row, col) in visited:
+            continue
+        if not row_inbounds or not col_inbounds:
+            continue
+
+        if grid[row][col] == 'S':
+            current_height = ord('a')
+        elif grid[row][col] == 'E':
+            current_height = ord('z')
+        else:
+            current_height = ord(grid[row][col])
+        if abs(current_height - height) > 1:
+            continue
+
+        if grid[row][col] == "E":
+            return distance
+
+        visited.add((row, col))
+
+        queue.append((row + 1, col, distance + 1, current_height))
+        queue.append((row - 1, col, distance + 1, current_height))
+        queue.append((row, col + 1, distance + 1, current_height))
+        queue.append((row, col - 1, distance + 1, current_height))
+
+    return -1
+
+
+def get_starting_point(grid: list) -> tuple:
+    rows = len(grid)
+    cols = len(grid[0])
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 'S':
+                return (row, col)
 
 
 def part1(inputFile: str) -> int:
-    monkeys = readInput(inputFile)
+    grid = readInput(inputFile)
+    row, col = get_starting_point(grid)
+    distance = explore(grid, row, col)
+    return distance
 
 
 def part2(inputFile: str):
@@ -27,7 +76,7 @@ def test():
     print('---- TEST ----')
     filename = 'test_input.txt'
 
-    assert part1(filename) == 10605
+    assert part1(filename) == 31
     print('Part 1 OK')
 
     # assert part2(filename) == 2713310158
@@ -39,7 +88,7 @@ def main():
     filename = 'input.txt'
 
     solution_part1 = part1(filename)
-    assert solution_part1 == 57348
+    # assert solution_part1 == 57348
     print(f'Solution for Part 1: {solution_part1}')
 
     # solution_part2 = part2(filename)
