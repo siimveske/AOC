f449b2cbae975cdbc6e9e1d13406c723d2007d53
Siim Veske <siim.veske@gmail.com>
2023-01-05 22:56:41 +0200

    day 16 part 1 solution

--- 2022/src/day16/day_16_proboscidea_volcanium.py
+++ 2022/src/day16/day_16_proboscidea_volcanium.py
@@ -1,5 +1,10 @@
 from __future__ import annotations
+from collections import defaultdict
+from itertools import product
+from math import inf as INFINITY
 import os
+
+'''https://github.com/mebeim/aoc/blob/master/2022/README.md#day-16---proboscidea-volcanium'''
 
 
 def readInput(filename: str):
@@ -7,26 +12,70 @@ def readInput(filename: str):
     script_location = os.path.dirname(os.path.realpath(__file__))
     input_file_path = os.path.join(script_location, filename)
 
-    data = {}
+    graph = {}
+    rates = {}
     with open(input_file_path, 'r') as f:
         for line in f:
             valve, neighbours = line.strip().split(';')
             valve_code = valve[6:8]
-            if valve_code == 'HH':
-                print()
             valve_flow_rate = int(valve[23:])
 
             neighbours = neighbours.strip()
             neighbours = neighbours.split(' ')[4:]
             neighbours = [n[0:2] for n in neighbours]
 
-            data[valve_code] = (valve_flow_rate, neighbours)
-    return data
+            graph[valve_code] = neighbours
+            rates[valve_code] = valve_flow_rate
+    return (graph, rates)
+
+
+def floyd_warshall(g):
+    distance = defaultdict(lambda: defaultdict(lambda: INFINITY))
+
+    for a, bs in g.items():
+        distance[a][a] = 0
+
+        for b in bs:
+            distance[a][b] = 1
+            distance[b][b] = 0
+
+    for a, b, c in product(g, g, g):
+        bc, ba, ac = distance[b][c], distance[b][a], distance[a][c]
+
+        if ba + ac < bc:
+            distance[b][c] = ba + ac
+
+    return distance
+
+
+def get_solutions(distances, valves, time=30, current_valve='AA', chosen_valves={}):
+    for nxt_valve in valves:
+        new_time = time - distances[current_valve][nxt_valve] - 1
+        if new_time < 2:
+            continue
+
+        new_chosen = chosen_valves | {nxt_valve: new_time}
+        yield from get_solutions(distances, valves - {nxt_valve}, new_time, nxt_valve, new_chosen)
+
+    yield chosen_valves
+
+
+def score(rates, valves):
+    result = 0
+    for valve, duration in valves.items():
+        result += rates[valve] * duration
+    return result
 
 
 def part1(inputFile: str) -> int:
-    data = readInput(inputFile)
-    return 0
+    graph, rates = readInput(inputFile)
+    distances = floyd_warshall(graph)
+    valves = frozenset(filter(rates.get, graph))
+
+    max_pressure = 0
+    for solution in get_solutions(distances, valves):
+        max_pressure = max(max_pressure, score(rates, solution))
+    return max_pressure
 
 
 def part2(inputFile: str):
@@ -38,10 +87,10 @@ def test():
     print('---- TEST ----')
     filename = 'test_input.txt'
 
-    assert part1(filename) == 26
+    assert part1(filename) == 1651
     print('Part 1 OK')
 
-    assert part2(filename) == 56000011
+    assert part2(filename) == 1707
     print('Part 2 OK\n')
 
 
@@ -50,7 +99,7 @@ def main():
     filename = 'input.txt'
 
     solution_part1 = part1(filename)
-    #assert solution_part1 == 4737443
+    assert solution_part1 == 2320
     print(f'Solution for Part 1: {solution_part1}')
 
     # solution_part2 = part2(filename)
