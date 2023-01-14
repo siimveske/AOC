aa28a28367e746e7099ea857a0a1ec69d5f0cf9f
Siim Veske <siim.veske@gmail.com>
2023-01-14 10:41:33 +0200

    solve day 18 part 1

--- 2022/src/day18/day_18_boiling_boulders.py
+++ 2022/src/day18/day_18_boiling_boulders.py
@@ -7,12 +7,31 @@ def readInput(filename: str):
     script_location = os.path.dirname(os.path.realpath(__file__))
     input_file_path = os.path.join(script_location, filename)
 
+    cubes = {}
     with open(input_file_path, 'r') as f:
-        return f.read()
+        for line in f:
+            coords = tuple(map(int, line.split(',')))
+            cubes[coords] = 6
+    return cubes
+
+
+def neighbours(x: int, y: int, z: int) -> tuple:
+    yield (x + 1, y, z)
+    yield (x - 1, y, z)
+    yield (x, y + 1, z)
+    yield (x, y + 1, z)
+    yield (x, y, z + 1)
+    yield (x, y, z - 1)
 
 
 def part1(inputFile: str) -> int:
-    pattern = readInput(inputFile)
+    cubes: dict = readInput(inputFile)
+    for cube in cubes:
+        for neighbour in neighbours(*cube):
+            if neighbour in cubes:
+                cubes[cube] -= 1
+
+    return sum(cubes.values())
 
 
 def part2(inputFile: str) -> int:
@@ -23,7 +42,7 @@ def test():
     print('---- TEST ----')
     filename = 'test_input.txt'
 
-    assert part1(filename) == 3068
+    assert part1(filename) == 64
     print('Part 1 OK')
 
     # assert part2(filename) == 1514285714288
