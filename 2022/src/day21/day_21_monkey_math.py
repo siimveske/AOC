10c968843fce9a3a5a50f1af4d9b8e391cf2883b
Siim Veske <siim.veske@gmail.com>
2023-01-20 11:31:29 +0200

    solve day 21 part 2

--- 2022/src/day21/day_21_monkey_math.py
+++ 2022/src/day21/day_21_monkey_math.py
@@ -1,11 +1,27 @@
 from operator import add, mul, sub, floordiv
 import os
 
+'''https://github.com/mebeim/aoc/tree/master/2022#day-21---monkey-math'''
+
 OPMAP = {
     '+': add,
     '*': mul,
     '-': sub,
     '/': floordiv
+}
+
+# Calculate R or L knowing that E = L op R.
+# True  = need to calculate L given E and R as parameters.
+# False = need to calculate R given E and L as parameters.
+REV_OPMAP = {
+    ('+', True): sub,
+    ('+', False): sub,
+    ('*', True): floordiv,
+    ('*', False): floordiv,
+    ('-', True): add,
+    ('-', False): lambda x, l: l - x,
+    ('/', True): mul,
+    ('/', False): lambda x, l: l // x,
 }
 
 
@@ -24,13 +40,36 @@ def readInput(filename: str):
 
 def calc(tree, node):
     value = tree[node]
-    if isinstance(value, int):
+    if not isinstance(value, list):
         return value
 
     l, op, r = value
     lvalue = calc(tree, l)
     rvalue = calc(tree, r)
+
+    if lvalue is None or rvalue is None:
+        return None
+
     return OPMAP[op](lvalue, rvalue)
+
+
+def find_value(tree, node, expected):
+    if node == 'humn':
+        # We reached the node we wanted and we now know its value.
+        return expected
+
+    l, op, r = tree[node]
+    lvalue = calc(tree, l)
+    rvalue = calc(tree, r)
+
+    if lvalue is None:
+        # Left side has unknown value, calculate its expected value and keep going.
+        expected = REV_OPMAP[op, True](expected, rvalue)
+        return find_value(tree, l, expected)
+
+    # Right side has unknown value, calculate its expected value and keep going.
+    expected = REV_OPMAP[op, False](expected, lvalue)
+    return find_value(tree, r, expected)
 
 
 def part1(inputFile: str):
@@ -40,7 +79,11 @@ def part1(inputFile: str):
 
 
 def part2(inputFile: str):
-    coordinates = readInput(inputFile)
+    tree = readInput(inputFile)
+    tree['humn'] = None
+    tree['root'][1] = '-'
+    answer = find_value(tree, 'root', 0)
+    return answer
 
 
 def test():
@@ -50,8 +93,8 @@ def test():
     assert part1(filename) == 152
     print('Part 1 OK')
 
-    # assert part2(filename) == 1623178306
-    # print('Part 2 OK')
+    assert part2(filename) == 301
+    print('Part 2 OK')
 
 
 def main():
@@ -60,11 +103,11 @@ def main():
 
     solution_part1 = part1(filename)
     print(f'Solution for Part 1: {solution_part1}')
-    # assert solution_part1 == 3346
+    assert solution_part1 == 331319379445180
 
-    # solution_part2 = part2(filename)
-    # print(f'Solution for Part 2: {solution_part2}\n')
-    # assert solution_part2 == 4265712588168
+    solution_part2 = part2(filename)
+    print(f'Solution for Part 2: {solution_part2}\n')
+    assert solution_part2 == 3715799488132
 
 
 if __name__ == '__main__':
