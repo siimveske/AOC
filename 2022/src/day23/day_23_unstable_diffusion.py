099359c54f943e4daa6b831cfd18b75f4805329a
Siim Veske <siim.veske@gmail.com>
2023-01-27 18:55:20 +0200

    start day 23

---
+++ 2022/src/day23/day_23_unstable_diffusion.py
@@ -0,0 +1,47 @@
+import os
+
+
+def readInput(filename: str):
+
+    script_location = os.path.dirname(os.path.realpath(__file__))
+    input_file_path = os.path.join(script_location, filename)
+
+    with open(input_file_path, 'r') as f:
+        pass
+
+
+def part1(inputFile: str):
+    readInput(inputFile)
+
+
+def part2(inputFile: str):
+    readInput(inputFile)
+
+
+def test():
+    print('---- TEST ----')
+    filename = 'test_input.txt'
+
+    assert part1(filename) == 110
+    print('Part 1 OK')
+
+    # assert part2(filename) == 5031
+    # print('Part 2 OK')
+
+
+def main():
+    print('\n---- MAIN ----')
+    filename = 'input.txt'
+
+    solution_part1 = part1(filename)
+    print(f'Solution for Part 1: {solution_part1}')
+    assert solution_part1 == 76332
+
+    # solution_part2 = part2(filename)
+    # print(f'Solution for Part 2: {solution_part2}\n')
+    # assert solution_part2 == 144012
+
+
+if __name__ == '__main__':
+    test()
+    main()
