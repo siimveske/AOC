c38877ce3d136d5002ec24c04e610bba4bf31958
Siim Veske <siim.veske@gmail.com>
2023-02-06 23:19:41 +0200

    solve day 4 part 2

--- 2015/day04/day_04_ideal_stocking_stuffer.py
+++ 2015/day04/day_04_ideal_stocking_stuffer.py
@@ -12,37 +12,34 @@ def readInput(filename: str):
         return f.read().strip()
 
 
-def part1(input: str) -> int:
-
-    if input.endswith('.txt'):
-        secret = readInput(input)
-    else:
-        secret = input
-
+def solve(prefix: str, secret: str) -> int:
     idx = 0
     while True:
         string = (secret + str(idx)).encode(encoding='utf-8')
         hash: str = hashlib.md5(string).hexdigest()
-        if hash.startswith('00000'):
+        if hash.startswith(prefix):
             return idx
         idx += 1
 
 
+def part1(inputFile: str) -> int:
+    secret = readInput(inputFile)
+    prefix = '00000'
+    return solve(prefix, secret)
+
+
 def part2(inputFile: str) -> int:
-    pass
+    secret = readInput(inputFile)
+    prefix = '000000'
+    return solve(prefix, secret)
 
 
 def test():
     print('---- TEST ----')
 
-    assert part1('abcdef') == 609043
-    assert part1('pqrstuv') == 1048970
-    print('Part 1 OK')
-
-    # assert len(travel2('^v')) == 3
-    # assert len(travel2('^>v<')) == 3
-    # assert len(travel2('^v^v^v^v^v')) == 11
-    # print('Part 2 OK\n')
+    assert solve('00000', 'abcdef') == 609043
+    assert solve('00000', 'pqrstuv') == 1048970
+    print('OK')
 
 
 def main():
@@ -52,8 +49,8 @@ def main():
     solution_part1 = part1(filename)
     print(f'Solution for Part 1: {solution_part1}')
 
-    # solution_part2 = part2(filename)
-    # print(f'Solution for Part 2: {solution_part2}\n')
+    solution_part2 = part2(filename)
+    print(f'Solution for Part 2: {solution_part2}\n')
 
 
 if __name__ == '__main__':
