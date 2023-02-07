cec5a927a79434aa6c035a1eb54828aa3e1bf300
Siim Veske <siim.veske@gmail.com>
2023-02-07 13:11:58 +0200

    solve day 6 part 2

--- 2015/day06/day_06_probably_a_fire_hazard.py
+++ 2015/day06/day_06_probably_a_fire_hazard.py
@@ -8,8 +8,16 @@ def readInput(filename: str):
     script_location = os.path.dirname(os.path.realpath(__file__))
     input_file_path = os.path.join(script_location, filename)
 
-    with open(input_file_path, 'r') as f:
-        return f.read().splitlines()
+    commands = []
+
+    if filename.endswith('txt'):
+        with open(input_file_path, 'r') as f:
+            for line in f:
+                commands.append(parse_line(line.strip()))
+    else:
+        commands.append(parse_line(filename))
+
+    return commands
 
 
 def parse_line(line: str) -> list:
@@ -21,15 +29,8 @@ def parse_line(line: str) -> list:
         return (parts[0], (x1, y1, x2, y2))
 
 
-def parse_lines(commands):
-    result = []
-    for cmd in commands:
-        result.append(parse_line(cmd))
-    return result
-
-
-def solve(commands: list) -> int:
-    grid = defaultdict(bool)
+def toggle_lights(commands: list) -> dict[tuple(int, int), int]:
+    lights = defaultdict(int)
     for cmd, args in commands:
         x1, y1, x2, y2 = args
         x_min = min(x1, x2)
@@ -40,32 +41,46 @@ def solve(commands: list) -> int:
             for col in range(x_min, x_max):
                 key = (row, col)
                 if cmd == 'on':
-                    grid[key] = True
+                    lights[key] = True
                 elif cmd == 'off':
-                    grid[key] = False
+                    lights[key] = False
                 else:
-                    grid[key] = not grid[key]
+                    lights[key] = not lights[key]
+    return lights
 
-    lights_on = 0
-    for light in grid.values():
-        if light == True:
-            lights_on += 1
 
-    return lights_on
+def toggle_lights2(commands: list) -> dict[tuple(int, int), int]:
+    lights = defaultdict(int)
+    for cmd, args in commands:
+        x1, y1, x2, y2 = args
+        x_min = min(x1, x2)
+        x_max = max(x1, x2) + 1
+        y_min = min(y1, y2)
+        y_max = max(y1, y2) + 1
+        for row in range(y_min, y_max):
+            for col in range(x_min, x_max):
+                key = (row, col)
+                if cmd == 'on':
+                    lights[key] += 1
+                elif cmd == 'off':
+                    lights[key] = max(0, lights[key] - 1)
+                else:
+                    lights[key] += 2
+    return lights
 
 
 def part1(inputFile: str) -> int:
-    if inputFile.endswith('txt'):
-        commands = readInput(inputFile)
-    else:
-        commands = [inputFile]
-
-    parsed_input = parse_lines(commands)
-    return solve(parsed_input)
+    commands = readInput(inputFile)
+    lights = toggle_lights(commands)
+    lights_on = sum(lights.values())
+    return lights_on
 
 
 def part2(inputFile: str) -> int:
-    pass
+    commands = readInput(inputFile)
+    lights = toggle_lights2(commands)
+    total_brightness = sum(lights.values())
+    return total_brightness
 
 
 def test():
@@ -76,12 +91,10 @@ def test():
     assert part1('turn off 499,499 through 500,500') == 0
     print('Part 1 OK')
 
-    # assert is_nice2('qjhvhtzxzqqjkmpb') == True
-    # assert is_nice2('xxyxx') == True
-    # assert is_nice2('uurcxstgmygtbstg') == False
-    # assert is_nice2('ieodomkazucvgmuy') == False
-    #
-    # print('Part 2 OK\n')
+    assert part2('turn on 0,0 through 0,0') == 1
+    assert part2('toggle 0,0 through 999,999') == 2_000_000
+
+    print('Part 2 OK\n')
 
 
 def main():
@@ -92,8 +105,9 @@ def main():
     print(f'Solution for Part 1: {solution_part1}')
     assert solution_part1 == 400410
 
-    # solution_part2 = part2(filename)
-    # print(f'Solution for Part 2: {solution_part2}\n')
+    solution_part2 = part2(filename)
+    print(f'Solution for Part 2: {solution_part2}\n')
+    assert solution_part2 == 15343601
 
 
 if __name__ == '__main__':
