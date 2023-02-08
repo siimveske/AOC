70a4c0fae839ad859e4823103346f5d82f61cd9a
Siim Veske <siim.veske@gmail.com>
2023-02-08 14:03:59 +0200

    solve day 8 part 2

--- 2015/day08/day_08_matchsticks.py
+++ 2015/day08/day_08_matchsticks.py
@@ -23,8 +23,34 @@ def part1(inputFile: str) -> int:
     return code_characters - value_characters
 
 
+def encode(text: str) -> str:
+    result = []
+    result.append('"')
+    for c in text:
+        if c == '"':
+            result.append(r'\"')
+        elif c == "'":
+            result.append(r"\'")
+        elif c == "\\":
+            result.append(r"\\")
+        else:
+            result.append(c)
+    result.append('"')
+    return ''.join(result)
+
+
 def part2(inputFile: str) -> int:
-    pass
+    data = readInput(inputFile)
+
+    code_characters = 0
+    encoded_characters = 0
+
+    for line in data:
+        code_characters += len(line)
+        tmp = encode(line)
+        encoded_characters += len(tmp)
+
+    return encoded_characters - code_characters
 
 
 def test():
@@ -32,6 +58,7 @@ def test():
 
     filename = 'test_input.txt'
     assert part1(filename) == 12
+    assert part2(filename) == 19
 
     print('OK\n')
 
@@ -44,9 +71,9 @@ def main():
     print(f'Solution for Part 1: {solution_part1}')
     assert solution_part1 == 1371
 
-    # solution_part2 = part2(filename)
-    # print(f'Solution for Part 2: {solution_part2}\n')
-    # assert solution_part2 == 14134
+    solution_part2 = part2(filename)
+    print(f'Solution for Part 2: {solution_part2}\n')
+    assert solution_part2 == 2117
 
 
 if __name__ == '__main__':
