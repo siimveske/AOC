b696cb73e53f5eec31347969d3125461c66072af
Siim Veske <siim.veske@gmail.com>
2023-02-11 17:39:11 +0200

    solve day 12 part 2

--- 2015/day12/day_12_jsabacus.py
+++ 2015/day12/day_12_jsabacus.py
@@ -2,9 +2,11 @@ from __future__ import annotations
 
 import os
 import re
+import json
+from typing import Iterable
 
 
-def readInput(filename: str):
+def readInput(filename: str) -> str:
     script_location = os.path.dirname(os.path.realpath(__file__))
     input_file_path = os.path.join(script_location, filename)
 
@@ -18,8 +20,32 @@ def part1(data: str) -> int:
     return result
 
 
-def part2(inputFile: str) -> int:
-    pass
+def travel(data: Iterable):
+    result = 0
+    if type(data) == list:
+        for i in data:
+            if type(i) == int:
+                result += i
+            elif type(i) in [dict, list]:
+                result += travel(i)
+
+    elif type(data) == dict:
+        for val in data.values():
+            if val == "red":
+                return 0
+            elif type(val) == int:
+                result += val
+            elif type(val) in [dict, list]:
+                result += travel(val)
+    else:
+        raise Exception(f"Unknown data type: {data}")
+    return result
+
+
+def part2(data: str) -> int:
+    json_data = json.loads(data)
+    result = travel(json_data)
+    return result
 
 
 def test():
@@ -34,6 +60,11 @@ def test():
     assert part1("[]") == 0
     assert part1("{}") == 0
 
+    assert part2("[1,2,3]") == 6
+    assert part2('[1,{"c":"red","b":2},3]') == 4
+    assert part2('{"d":"red","e":[1,2,3,4],"f":5}') == 0
+    assert part2('[1,"red",5]') == 6
+
     print("OK\n")
 
 
@@ -45,11 +76,11 @@ def main():
 
     solution_part1 = part1(data)
     print(f"Solution for Part 1: {solution_part1}")
-    # assert solution_part1 == 141
+    assert solution_part1 == 111754
 
-    # solution_part2 = part2(filename)
-    # print(f"Solution for Part 2: {solution_part2}\n")
-    # assert solution_part2 == 736
+    solution_part2 = part2(data)
+    print(f"Solution for Part 2: {solution_part2}\n")
+    assert solution_part2 == 65402
 
 
 if __name__ == "__main__":
