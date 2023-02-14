872fe6c0467fad7ccd2ba9da2fa23af38b6a4101
Siim Veske <siim.veske@gmail.com>
2023-02-14 18:39:16 +0200

    solve day 14 part 2

--- 2015/day14/day_14_reindeer_olympics.py
+++ 2015/day14/day_14_reindeer_olympics.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
 import os
-from itertools import permutations
+from collections import defaultdict
 
 
-def readInput(filename: str):
+def readInput(filename: str) -> list:
     script_location = os.path.dirname(os.path.realpath(__file__))
     input_file_path = os.path.join(script_location, filename)
 
@@ -20,7 +20,7 @@ def readInput(filename: str):
     return reindeers
 
 
-def calculate_distances(reindeers, travel_time):
+def calculate_distances(reindeers, travel_time) -> dict:
     distances = dict()
     for name, speed, duration, pause in reindeers:
         period = duration + pause
@@ -33,21 +33,31 @@ def calculate_distances(reindeers, travel_time):
     return distances
 
 
-def part1(inputFile: str) -> int:
-    reindeers = readInput(inputFile)
-    distances = calculate_distances(reindeers, travel_time=2503)
+def part1(reindeers: list, duration=2503) -> int:
+    distances = calculate_distances(reindeers, travel_time=duration)
     return max(distances.values())
 
 
-def part2(inputFile: str) -> int:
-    graph = readInput(inputFile)
+def part2(reindeers: list, duration=2503) -> int:
+    points = defaultdict(int)
+    for i in range(duration):
+        distances = calculate_distances(reindeers, travel_time=i + 1)
+        max_dist = max(distances.values())
+        for reindeer in distances:
+            if distances[reindeer] == max_dist:
+                points[reindeer] += 1
+    winner = max(points.values())
+    return winner
 
 
 def test():
     print("---- TEST ----")
 
     filename = "test_input.txt"
-    # assert part1(filename) == 330
+    data = readInput(filename)
+
+    assert part1(data, duration=1000) == 1120
+    assert part2(data, duration=1000) == 689
 
     print("OK\n")
 
@@ -55,14 +65,15 @@ def test():
 def main():
     print("---- MAIN ----")
     filename = "input.txt"
+    data = readInput(filename)
 
-    solution_part1 = part1(filename)
+    solution_part1 = part1(data)
     print(f"Solution for Part 1: {solution_part1}")
-    # assert solution_part1 == 618
+    assert solution_part1 == 2660
 
-    # solution_part2 = part2(filename)
-    # print(f"Solution for Part 2: {solution_part2}\n")
-    # assert solution_part2 == 601
+    solution_part2 = part2(data)
+    print(f"Solution for Part 2: {solution_part2}\n")
+    assert solution_part2 == 1256
 
 
 if __name__ == "__main__":
