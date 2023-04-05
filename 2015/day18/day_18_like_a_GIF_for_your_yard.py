3c072cfc7bdb87746611a8c9c8596ecb6798f206
Siim Veske <siim.veske@gmail.com>
2023-04-05 22:39:49 +0300

    refactor day 18

--- 2015/day18/day_18_like_a_GIF_for_your_yard.py
+++ 2015/day18/day_18_like_a_GIF_for_your_yard.py
@@ -5,30 +5,20 @@ def readInput(filename: str) -> list[list[bool]]:
     script_location = os.path.dirname(os.path.realpath(__file__))
     input_file_path = os.path.join(script_location, filename)
 
-    grid = []
     with open(input_file_path, "r") as f:
-        for  line in f:
-            grid.append([1 if i=='#' else 0 for i in line.strip()])
+        grid = [[1 if char == '#' else 0 for char in line.strip()] for line in f]
     return grid
 
 
 def part1_solver(grid: list[list[int]], row: int, col: int) -> int:
     neighbours = [
-        [row-1, col],
-        [row-1, col+1],
-        [row, col+1],
-        [row+1, col+1],
-        [row+1, col],
-        [row+1, col-1],
-        [row, col-1],
-        [row-1, col-1]
+        [row-1, col-1], [row-1, col], [row-1, col+1],
+        [row,   col-1],               [row,   col+1],
+        [row+1, col-1], [row+1, col], [row+1, col+1],
     ]
 
-    lights_on = 0
-    rows=cols=len(grid)
-    for r,c in neighbours:
-        if 0 <= r < rows and 0 <= c < cols:
-            lights_on += grid[r][c]
+    grid_size = len(grid)
+    lights_on = sum(grid[r][c] for r,c in neighbours if 0 <= r < grid_size and 0 <= c < grid_size)
 
     if grid[row][col]:
         return 1 if lights_on in [2,3] else 0
@@ -37,44 +27,32 @@ def part1_solver(grid: list[list[int]], row: int, col: int) -> int:
 
 
 def part2_solver(grid: list[list[int]], row: int, col: int) -> int:
-    rows = cols = len(grid) - 1
-    if (row, col) in [(0, 0), (0, cols), (rows, 0), (rows, cols)]:
+    edge = len(grid) - 1
+    if (row, col) in [(0, 0), (0, edge), (edge, 0), (edge, edge)]:
         return 1
     return part1_solver(grid, row, col)
 
 
 def evolve(grid: list, solver: callable):
-    steps = 100
-    rows=cols=len(grid)
-    for _ in range(steps):
-        new_grid = []
-        for row in range(rows):
-            new_row = []
-            for col in range(cols):
-                item = solver(grid, row, col)
-                new_row.append(item)
-            new_grid.append(new_row)
-        grid = new_grid
+    iterations = 100
+    grid_size = len(grid)
+    for _ in range(iterations):
+        grid = [[solver(grid, row, col) for col in range(grid_size)] for row in range(grid_size)]
     return grid
 
 
 def part1(grid: list[list[int]]) -> int:
     grid = evolve(grid, part1_solver)
-    result = 0
-    for row in grid:
-        for col in row:
-            result += col
-    return result
+    return sum(sum(grid, []))
 
 
 def part2(grid: list[list[int]]) -> int:
-    grid[0][0] = grid[0][99] = grid[99][0] = grid[99][99] = 1 # initialize four corners
+    # initialize four always ON corners
+    grid_size = len(grid) - 1
+    grid[0][0] = grid[0][grid_size] = grid[grid_size][0] = grid[grid_size][grid_size] = 1
+
     grid = evolve(grid, part2_solver)
-    result = 0
-    for row in grid:
-        for col in row:
-            result += col
-    return result
+    return sum(sum(grid, []))
 
 def main():
     print("---- MAIN ----")
