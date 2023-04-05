ea721d72148a6a13a599b5d003792087c6d6d386
Siim Veske <siim.veske@gmail.com>
2023-04-05 18:53:19 +0300

    save not working solution

--- 2015/day18/day_18_like_a_GIF_for_your_yard.py
+++ 2015/day18/day_18_like_a_GIF_for_your_yard.py
@@ -12,8 +12,7 @@ def readInput(filename: str) -> list[list[bool]]:
     return grid
 
 
-def eval_state(grid: list[list[int]], row: int, col: int) -> int:
-    rows=cols=len(grid)
+def part1_solver(grid: list[list[int]], row: int, col: int) -> int:
     neighbours = [
         [row-1, col],
         [row-1, col+1],
@@ -26,6 +25,7 @@ def eval_state(grid: list[list[int]], row: int, col: int) -> int:
     ]
 
     lights_on = 0
+    rows=cols=len(grid)
     for r,c in neighbours:
         if 0 <= r < rows and 0 <= c < cols:
             lights_on += grid[r][c]
@@ -36,7 +36,14 @@ def eval_state(grid: list[list[int]], row: int, col: int) -> int:
         return 1 if lights_on == 3 else 0
 
 
-def part1(grid: list[list[int]]) -> int:
+def part2_solver(grid: list[list[int]], row: int, col: int) -> int:
+    rows = cols = len(grid) - 1
+    if (row, col) in [(0, 0), (0, cols), (rows, 0), (rows, cols)]:
+        return 1
+    return part1_solver(grid, row, col)
+
+
+def evolve(grid: list, solver: callable):
     steps = 100
     rows=cols=len(grid)
     for _ in range(steps):
@@ -44,11 +51,15 @@ def part1(grid: list[list[int]]) -> int:
         for row in range(rows):
             new_row = []
             for col in range(cols):
-                item = eval_state(grid, row, col)
+                item = solver(grid, row, col)
                 new_row.append(item)
             new_grid.append(new_row)
         grid = new_grid
+    return grid
 
+
+def part1(grid: list[list[int]]) -> int:
+    grid = evolve(grid, part1_solver)
     result = 0
     for row in grid:
         for col in row:
@@ -56,10 +67,13 @@ def part1(grid: list[list[int]]) -> int:
     return result
 
 
-
-def part2(containers: dict) -> int:
-    pass
-
+def part2(grid: list[list[int]]) -> int:
+    grid = evolve(grid, part2_solver)
+    result = 0
+    for row in grid:
+        for col in row:
+            result += col
+    return result
 
 def main():
     print("---- MAIN ----")
@@ -70,8 +84,8 @@ def main():
     print(f"Solution for Part 1: {solution_part1}")
     assert solution_part1 == 814
 
-    # solution_part2 = part2(data)
-    # print(f"Solution for Part 2: {solution_part2}\n")
+    solution_part2 = part2(data)
+    print(f"Solution for Part 2: {solution_part2}\n")
     # assert solution_part2 == 17
 
 
