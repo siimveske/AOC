5a17301aad83f5cfafebb589793b1b544e78f99d
Siim Veske <siim.veske@gmail.com>
2023-12-01 10:41:23 +0200

    Solve 2023 day 1 part 1

---
+++ 2023/day1/day_01_trebuchet.py
@@ -0,0 +1,65 @@
+import os
+
+
+def read_input(filename: str) -> list[str]:
+    script_location = os.path.dirname(os.path.realpath(__file__))
+    input_file_path = os.path.join(script_location, filename)
+
+    with open(input_file_path, 'r') as f:
+        lines = [n.strip() for n in f.readlines()]
+
+    return lines
+
+
+def get_first_number(line: str) -> str:
+    for character in line:
+        if character.isdigit():
+            return character
+
+
+def get_last_number(line: str) -> str:
+    for character in line[::-1]:
+        if character.isdigit():
+            return character
+
+
+def part1(input_file: str) -> int:
+    lines = read_input(input_file)
+
+    total = 0
+    for line in lines:
+        a = get_first_number(line)
+        b = get_last_number(line)
+        total += int(a+b)
+    return total
+
+
+def part2(input_file: str):
+    pass
+
+
+def test():
+    print('---- TEST ----')
+    filename = 'test_input.txt'
+
+    assert part1(filename) == 142
+    print('Part 1 OK')
+
+    # assert part2(filename) == 45000
+    # print('Part 2 OK')
+
+
+def main():
+    print('\n---- MAIN ----')
+    filename = 'input.txt'
+
+    solution_part1 = part1(filename)
+    print(f'Solution for Part 1: {solution_part1}')
+
+    # solution_part2 = part2(filename)
+    # print(f'Solution for Part 2: {solution_part2}\n')
+
+
+if __name__ == '__main__':
+    test()
+    main()
