478e4bf3edbeb3930b0a49b79db069a985c3c90a
Siim Veske <siim.veske@gmail.com>
2023-12-04 02:13:51 +0200

    solve 2023 day 3 part 2

--- 2023/day3/day_03_gear_ratios.py
+++ 2023/day3/day_03_gear_ratios.py
@@ -1,5 +1,7 @@
+import math
 import os
 import re
+from collections import defaultdict
 
 
 def read_input(filename: str) -> list:
@@ -47,8 +49,49 @@ def part1(input_file: str) -> int:
     return sum_of_part_numbers
 
 
+def get_neighbours(row: int, col: int):
+    top = (row - 1, col)
+    top_right = (row - 1, col + 1)
+    right = (row, col + 1)
+    bottom_right = (row + 1, col + 1)
+    bottom = (row + 1, col)
+    bottom_left = (row + 1, col - 1)
+    left = (row, col - 1)
+    top_left = (row - 1, col - 1)
+    return [top, top_right, right, bottom_right, bottom, bottom_left, left, top_left]
+
+
 def part2(input_file: str) -> int:
-    pass
+    engine_schematic = read_input(input_file)
+
+    stars = []
+    numbers = defaultdict(list)
+    digit_pattern = re.compile(r'\d+')
+    star_pattern = re.compile(r'\*')
+    for row, line in enumerate(engine_schematic):
+        for match in digit_pattern.finditer(line):
+            value = int(match.group())
+            start_col, end_col = match.span()
+            numbers[row].append(([(row, i) for i in range(start_col, end_col)], value))
+        for match in star_pattern.finditer(line):
+            column, _ = match.span()
+            location = (row, column)
+            stars.append(location)
+
+    sum_of_gear_ratios = 0
+    for r, c in stars:
+        candidates = [numbers[i] for i in range(r - 1, r + 2) if i in numbers]
+        neighbours = get_neighbours(r, c)
+        gear_set = set()
+        for neighbour in neighbours:
+            for i in candidates:
+                for j in i:
+                    if neighbour in j[0]:
+                        gear_set.add(j[1])
+        if len(gear_set) == 2:
+            sum_of_gear_ratios += math.prod(gear_set)
+
+    return sum_of_gear_ratios
 
 
 def test():
@@ -58,8 +101,8 @@ def test():
     assert part1(filename) == 4361
     print('Part 1 OK')
 
-    # assert part2(filename) == 2286
-    # print('Part 2 OK')
+    assert part2(filename) == 467835
+    print('Part 2 OK')
 
 
 def main():
@@ -69,11 +112,11 @@ def main():
     solution_part1 = part1(filename)
     print(f'Solution for Part 1: {solution_part1}')
 
-    # solution_part2 = part2(filename)
-    # print(f'Solution for Part 2: {solution_part2}\n')
+    solution_part2 = part2(filename)
+    print(f'Solution for Part 2: {solution_part2}\n')
 
     assert solution_part1 == 549908
-    # assert solution_part2 == 72422
+    assert solution_part2 == 81166799
 
 
 if __name__ == '__main__':
