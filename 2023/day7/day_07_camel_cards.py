3d4c5be4c2f9f0b7518127442989add7a6e9de82
Siim Veske <siim.veske@gmail.com>
2023-12-11 15:46:49 +0200

    solve 2023 day 7 part 2

--- 2023/day7/day_07_camel_cards.py
+++ 2023/day7/day_07_camel_cards.py
@@ -1,23 +1,9 @@
 import os
 from collections import Counter
+from typing import Any
 
 
 class Hand:
-    CARD = {
-        '2': 2,
-        '3': 3,
-        '4': 4,
-        '5': 5,
-        '6': 6,
-        '7': 7,
-        '8': 8,
-        '9': 9,
-        'T': 10,
-        'J': 11,
-        'Q': 12,
-        'K': 13,
-        'A': 14
-    }
     STRENGTH = {
         'high-card': 0,
         'one-pair': 1,
@@ -28,81 +14,117 @@ class Hand:
         'five-of-a-kind': 6
     }
 
-    def __init__(self, cards: str, bid: str):
+    def __init__(self, cards: str, bid: str, joker=False):
+        self.card_scores = {
+            '2': 2,
+            '3': 3,
+            '4': 4,
+            '5': 5,
+            '6': 6,
+            '7': 7,
+            '8': 8,
+            '9': 9,
+            'T': 10,
+            'J': 11,
+            'Q': 12,
+            'K': 13,
+            'A': 14
+        }
         self.bid = int(bid)
         self.cards = cards
-        self.type = Hand._parse_type(cards)
+        self.joker = joker
+        if joker:
+            self.card_scores['J'] = 1
+        self.type = self._parse_type()
         self.strength = Hand.STRENGTH[self.type]
 
-    def __eq__(self, other):
-        return self.cards == other.cards
-
     def __lt__(self, other):
-        if self.strength < other.strength:
-            return True
-        if self.strength > other.strength:
+        if self.cards == other.cards:
             return False
+
+        if self.strength != other.strength:
+            return self.strength < other.strength
+
         for a, b in zip(self.cards, other.cards):
             if a == b:
                 continue
-            return Hand.CARD[a] < Hand.CARD[b]
-        return False
+            return self.card_scores[a] < self.card_scores[b]
 
-    def __gt__(self, other):
-        if self.strength > other.strength:
-            return True
-        if self.strength < other.strength:
-            return False
-        for a, b in zip(self.cards, other.cards):
-            if a == b:
-                continue
-            return Hand.CARD[a] > Hand.CARD[b]
-        return False
+        assert False
 
-    @staticmethod
-    def _parse_type(cards: str):
-        c = Counter(cards)
-        c_val = list(c.values())
-        if 5 in c_val:
-            h_type = 'five-of-a-kind'
-        elif 4 in c_val:
-            h_type = 'four-of-a-kind'
-        elif 3 in c_val and 2 in c_val:
-            h_type = 'full-house'
-        elif 3 in c_val and c_val.count(1) == 2:
-            h_type = 'three-of-a-kind'
-        elif c_val.count(2) == 2 and c_val.count(1) == 1:
-            h_type = 'two-pair'
-        elif c_val.count(2) == 1 and c_val.count(1) == 3:
-            h_type = 'one-pair'
-        elif c_val.count(1) == 5:
-            h_type = 'high-card'
-        else:
-            raise Exception(f'Unknown hand: {cards}')
-        return h_type
+    def _parse_type(self):
+        cards = self.cards  # we will replace value of joker
+        ordered_counts = Counter(cards).most_common()  # e.g. [('3', 2), ('2', 1), ('T', 1), ('K', 1)]
+
+        # get the most common card
+        best, best_count = ordered_counts[0]  # e.g. ('3', 2)
+        if best_count == 5:
+            return "five-of-a-kind"
+
+        second_best, second_best_count = ordered_counts[1]  # e.g. ('2', 1)
+
+        if self.joker:  # Part 2
+            if best == 'J':  # get the next best card
+                best, best_count = second_best, second_best_count
+
+            # convert all J into the best card
+            cards = cards.replace("J", best)
+
+            ordered_counts = Counter(cards).most_common()  # recount the hand
+            best, best_count = ordered_counts[0]
+            if best_count == 5:
+                return "five-of-a-kind"
+
+            second_best, second_best_count = ordered_counts[1]  # e.g. ('2', 1)
+
+        if best_count == 4:
+            return "four-of-a-kind"
+
+        if best_count == 3:
+            if second_best_count == 2:
+                return "full-house"
+            else:
+                return "three-of-a-kind"
+
+        if best_count == 2:
+            if second_best_count == 2:
+                return "two-pair"
+            else:
+                return "one-pair"
+
+        return "high-card"
 
 
-def read_input(filename: str) -> list[Hand]:
+def read_input(filename: str) -> list[tuple[Any, Any]]:
     script_location = os.path.dirname(os.path.realpath(__file__))
     input_file_path = os.path.join(script_location, filename)
 
+    data = []
     with open(input_file_path, 'r') as f:
         for line in f:
-            hands = [Hand(cards, bid) for cards, bid in line.split()]
-    return hands
+            cards, bid = line.split()
+            data.append((cards, bid))
+    return data
 
 
 def part1(input_file: str) -> int:
-    hands = read_input(input_file)
+    data = read_input(input_file)
+    hands = [Hand(cards, bid) for cards, bid in data]
     hands.sort()
     result = 0
-    for idx, hand in enumerate(hands):
-        result += (idx + 1) * hand.bid
+    for idx, hand in enumerate(hands, 1):
+        result += (idx * hand.bid)
     return result
 
 
 def part2(input_file: str) -> int:
-    pass
+    data = read_input(input_file)
+    hands = [Hand(cards, bid, joker=True) for cards, bid in data]
+    hands.sort()
+    result = 0
+    for idx, hand in enumerate(hands, 1):
+        result += idx * hand.bid
+    return result
 
 
 def test():
@@ -112,8 +134,8 @@ def test():
     assert part1(filename) == 6440
     print('Part 1 OK')
 
-    # assert part2(filename) == 71503
-    # print('Part 2 OK')
+    assert part2(filename) == 5905
+    print('Part 2 OK')
 
 
 def main():
@@ -123,11 +145,11 @@ def main():
     solution_part1 = part1(filename)
     print(f'Solution for Part 1: {solution_part1}')
 
-    # solution_part2 = part2(filename)
-    # print(f'Solution for Part 2: {solution_part2}\n')
+    solution_part2 = part2(filename)
+    print(f'Solution for Part 2: {solution_part2}\n')
 
     assert solution_part1 == 253954294
-    # assert solution_part2 == 41382569
+    assert solution_part2 == 254837398
 
 
 if __name__ == '__main__':
