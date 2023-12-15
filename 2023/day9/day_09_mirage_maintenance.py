ebaddd3a2d03f5f3274b908777be9cacb1b5d8af
Siim Veske <siim.veske@gmail.com>
2023-12-15 02:15:08 +0200

    Solve 2023 day 9 part 1

---
+++ 2023/day9/day_09_mirage_maintenance.py
@@ -0,0 +1,74 @@
+import math
+import os
+from itertools import cycle
+from typing import List
+
+
+def read_input(filename: str) -> list[list[int]]:
+    script_location = os.path.dirname(os.path.realpath(__file__))
+    input_file_path = os.path.join(script_location, filename)
+
+    data = []
+    with open(input_file_path, 'r') as f:
+        for line in f:
+            numbers = [int(i) for i in line.split()]
+            data.append(numbers)
+    return data
+
+
+def part1(input_file: str) -> int:
+    data = read_input(input_file)
+    total = 0
+
+    for line in data:
+        sequence = [line]
+
+        while True:
+            differences = [sequence[-1][i + 1] - sequence[-1][i] for i in range(len(sequence[-1]) - 1)]
+
+            if all(diff == 0 for diff in differences):
+                cumulative_sum = [0]
+                while sequence:
+                    last_element = sequence.pop()[-1]
+                    cumulative_sum.append(cumulative_sum[-1] + last_element)
+                total += cumulative_sum[-1]
+                break
+            else:
+                sequence.append(differences)
+
+    return total
+
+
+def part2(input_file: str) -> int:
+    directions, graph = read_input(input_file)
+
+
+def test():
+    print('---- TEST ----')
+    filename = 'test_input.txt'
+
+    assert part1(filename) == 114
+    print('Part 1 OK')
+
+    # filename = 'test_input2.txt'
+    # assert part2(filename) == 6
+    # print('Part 2 OK')
+
+
+def main():
+    print('\n---- MAIN ----')
+    filename = 'input.txt'
+
+    solution_part1 = part1(filename)
+    print(f'Solution for Part 1: {solution_part1}')
+
+    # solution_part2 = part2(filename)
+    # print(f'Solution for Part 2: {solution_part2}\n')
+
+    assert solution_part1 == 1953784198
+    # assert solution_part2 == 18024643846273
+
+
+if __name__ == '__main__':
+    test()
+    main()
