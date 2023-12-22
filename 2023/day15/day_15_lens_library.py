6d93ad8ab75f416d518fed8dabc0755e2f437f13
Siim Veske <siim.veske@gmail.com>
2023-12-23 00:28:03 +0200

    Solve 2023 day 15 part 1

--- 2023/day15/day_15_lens_library.py
+++ 2023/day15/day_15_lens_library.py
@@ -1,18 +1,26 @@
 import os
 
 
-def read_input(filename: str) -> list[list[str]]:
+def read_input(filename: str) -> list[str]:
     script_location = os.path.dirname(os.path.realpath(__file__))
     input_file_path = os.path.join(script_location, filename)
 
     with open(input_file_path, 'r') as f:
-        lines = [list(n.strip()) for n in f]
-
-    return lines
+        return f.readline().split(',')
 
 
 def part1(input_file: str) -> int:
-    lines = read_input(input_file)
+    data = read_input(input_file)
+    result = 0
+    for step in data:
+        current_value = 0
+        for character in step:
+            current_value += ord(character)
+            current_value *= 17
+            current_value = current_value % 256
+        result += current_value
+
+    return result
 
 
 def part2(input_file: str) -> int:
@@ -23,7 +31,7 @@ def test():
     print('---- TEST ----')
     filename = 'test_input.txt'
 
-    assert part1(filename) == 136
+    assert part1(filename) == 1320
     print('Part 1 OK')
 
     # assert part2(filename) == 64
@@ -39,8 +47,8 @@ def main():
 
     # solution_part2 = part2(filename)
     # print(f'Solution for Part 2: {solution_part2}\n')
-    #
-    # assert solution_part1 == 106648
+
+    assert solution_part1 == 510792
     # assert solution_part2 == 87700
 
 
