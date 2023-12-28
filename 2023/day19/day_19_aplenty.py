d8ec1d9244b1315d7b2082f516bd7c97b48f1353
Siim Veske <siim.veske@gmail.com>
2023-12-28 23:28:08 +0200

    Solve 2023 day 19 part 1

--- 2023/day19/day_19_aplenty.py
+++ 2023/day19/day_19_aplenty.py
@@ -1,21 +1,67 @@
+import collections
 import os
+import re
 
 
-def read_input(filename: str) -> list[tuple[str, int, str]]:
+def read_input(filename: str):
     script_location = os.path.dirname(os.path.realpath(__file__))
     input_file_path = os.path.join(script_location, filename)
 
-    tokens = []
     with open(input_file_path, 'r') as f:
-        lines = f.read().splitlines()
-        for line in lines:
-            direction, value, color = line.split()
-            tokens.append((direction, int(value), color))
-    return tokens
+        workflows, ratings = f.read().split('\n\n')
+        workflows = workflows.splitlines()
+        ratings = ratings.splitlines()
+
+        xmas_workflows = {}
+        for workflow in workflows:
+            name, cmd = workflow.split('{')
+            cmd = cmd[:-1]
+            cmd = cmd.split(',')
+            xmas_workflows[name] = cmd
+
+        xmas_pattern = r"{x=(\d+),m=(\d+),a=(\d+),s=(\d+)}"
+        xmas_ratings = []
+        for rating in ratings:
+            match = re.search(xmas_pattern, rating)
+            x, m, a, s = match.groups()
+            xmas_ratings.append({'x': int(x), 'm': int(m), 'a': int(a), 's': int(s)})
+
+        return xmas_ratings, xmas_workflows
 
 
 def part1(input_file: str) -> int:
-    tokens = read_input(input_file)
+    total = 0
+    xmas_ratings, xmas_workflows = read_input(input_file)
+    for rating in xmas_ratings:
+        queue = collections.deque(xmas_workflows['in'])
+        while queue:
+            cmd = queue.popleft()
+            if cmd == 'R':
+                break
+            elif cmd == 'A':
+                total += sum(rating.values())
+                break
+            elif '<' in cmd:
+                condition, next_flow = cmd.split(':')
+                letter, val = condition.split('<')
+                if rating[letter] < int(val):
+                    queue.clear()
+                    if next_flow in xmas_workflows:
+                        queue.extend(xmas_workflows[next_flow])
+                    else:
+                        queue.extend(next_flow)
+            elif '>' in cmd:
+                condition, next_flow = cmd.split(':')
+                letter, val = condition.split('>')
+                if rating[letter] > int(val):
+                    queue.clear()
+                    if next_flow in xmas_workflows:
+                        queue.extend(xmas_workflows[next_flow])
+                    else:
+                        queue.extend(next_flow)
+            else:
+                queue.extend(xmas_workflows[cmd])
+    return total
 
 
 def part2(input_file: str) -> int:
@@ -42,8 +88,8 @@ def main():
 
     # solution_part2 = part2(filename)
     # print(f'Solution for Part 2: {solution_part2}\n')
-    #
-    # assert solution_part1 == 47527
+
+    assert solution_part1 == 323625
     # assert solution_part2 == 52240187443190
 
 
