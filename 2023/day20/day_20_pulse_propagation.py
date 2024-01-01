683dfcecbadde57350312d5674708cb3de1853f7
Siim Veske <siim.veske@gmail.com>
2024-01-01 14:34:16 +0200

    Solve 2023 day 20 part 1

--- 2023/day20/day_20_pulse_propagation.py
+++ 2023/day20/day_20_pulse_propagation.py
@@ -1,85 +1,36 @@
 import collections
 import os
-import re
-from copy import deepcopy
+from enum import Enum
 
 
-class Conjunction:
-    def __init__(self, name, outputs: list):
-        self.name = name
+class ModuleType(Enum):
+    FLIPFLOP = 1
+    CONJUNCTION = 2
+    BROADCASTER = 3
+
+
+class Module:
+    def __init__(self, module_name: str, module_type: ModuleType, outputs: list[str]):
+        self.name = module_name
+        self.type = module_type
         self.outputs = outputs
-        self.inputs = {}
-        self.pulse_cnt = {0: 0, 1: 0}
 
-    def set_pulse(self, input_name, pulse):
-        self.inputs[input_name] = pulse
+        if self.type == ModuleType.FLIPFLOP:
+            self.memory = False
+        elif self.type == ModuleType.CONJUNCTION:
+            self.memory = {}
 
-    def process(self):
-        if all(i == 1 for i in self.inputs.values()):
-            for output in self.outputs:
-                output.set_pulse(0)
-                self.pulse_cnt[0] += 1
-        else:
-            for output in self.outputs:
-                output.set_pulse(1)
-                self.pulse_cnt[1] += 1
+    def update_state(self, sender: str, pulse: bool):
+        if self.type == ModuleType.FLIPFLOP:
+            if not pulse:
+                self.memory = not self.memory
+                return self.memory
 
+        elif self.type == ModuleType.CONJUNCTION:
+            self.memory[sender] = pulse
+            return False if all([i for i in self.memory.values()]) else True
 
-class FlipFlop:
-    def __init__(self, name, outputs: list):
-        self.name = name
-        self.outputs = outputs
-        self.is_on = False
-        self.pulse = None
-        self.pulse_cnt = {0: 0, 1: 0}
-
-    def set_pulse(self, pulse):
-        self.pulse = pulse
-
-    def process(self):
-        if self.pulse == 1:
-            return []
-
-        if self.is_on:
-            self.is_on = False
-            for output in self.outputs:
-                output.set_pulse(0)
-                self.pulse_cnt[0] += 1
-        else:
-            self.is_on = True
-            for output in self.outputs:
-                output.set_pulse(1)
-                self.pulse_cnt[1] += 1
-        return self.outputs
-
-
-class Broadcaster:
-    def __init__(self, outputs: list):
-        self.name = 'broadcaster'
-        self.outputs = outputs
-        self.pulse = None
-        self.pulse_cnt = {0: 0, 1: 0}
-
-    def set_pulse(self, pulse):
-        self.pulse = pulse
-
-    def process(self):
-        for output in self.outputs:
-            output.set_pulse(self.pulse)
-            self.pulse_cnt[self.pulse] += 1
-        return self.outputs
-
-
-class Button:
-    def __init__(self, broadcaster):
-        self.name = 'button'
-        self.broadcaster = broadcaster
-        self.pulse_cnt = {0: 0, 1: 0}
-
-    def press(self):
-        self.broadcaster.set_pulse(0)
-        self.pulse_cnt[0] += 1
-        return self.broadcaster
+        return None
 
 
 def read_input(filename: str):
@@ -91,28 +42,51 @@ def read_input(filename: str):
         for line in f:
             line = line.strip()
             name, outputs = line.split(' -> ')
-            if ', ' in outputs:
-                outputs = outputs.split(', ')
-            else:
-                outputs = [outputs]
+            prefix = name[0]
+            name = name[1:]
+            outputs = outputs.split(', ')
+            if prefix == 'b':
+                graph['broadcaster'] = Module('broadcaster', ModuleType.BROADCASTER, outputs)
+            elif prefix == '%':
+                graph[name] = Module(name, ModuleType.FLIPFLOP, outputs)
+            elif prefix == '&':
+                graph[name] = Module(name, ModuleType.CONJUNCTION, outputs)
 
-            if name == 'broadcaster':
-                graph['broadcaster'] = Broadcaster(outputs)
-            elif name.startswith('%'):
-                name = name[1:]
-                flipflop = FlipFlop(name, outputs)
-                graph[name] = flipflop
-            else:
-                name = name[1:]
-                conjunction = Conjunction(name, outputs)
-                graph[name] = conjunction
-        graph['button'] = Button(graph['broadcaster'])
+    for name, module in graph.items():
+        for output in module.outputs:
+            if output in graph and graph[output].type == ModuleType.CONJUNCTION:
+                graph[output].memory[name] = False
+
     return graph
 
 
 def part1(input_file: str) -> int:
     graph = read_input(input_file)
-    btn = graph.get('button')
+    broadcaster = graph.get('broadcaster')
+
+    low, high = 0, 0
+    for i in range(1000):
+        low += 1
+        q = collections.deque([('broadcaster', destination, False) for destination in broadcaster.outputs])
+        while q:
+            src, dst, pulse = q.popleft()
+            # print(f'{src} -> {pulse} -> {dst}')
+            if pulse:
+                high += 1
+            else:
+                low += 1
+
+            # skip the 'output' debug node
+            if dst not in graph:
+                continue
+
+            module = graph[dst]
+            out_pulse = module.update_state(src, pulse)
+            if out_pulse is not None:
+                for output in module.outputs:
+                    q.append((module.name, output, out_pulse))
+
+    return low * high
 
 
 def part2(input_file: str) -> int:
@@ -143,8 +117,8 @@ def main():
 
     # solution_part2 = part2(filename)
     # print(f'Solution for Part 2: {solution_part2}\n')
-    #
-    # assert solution_part1 == 323625
+
+    assert solution_part1 == 898557000
     # assert solution_part2 == 127447746739409
 
 
