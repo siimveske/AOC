177afb12953c5c3bddd4dc2ed06eddcc8c616010
Siim Veske <siim.veske@gmail.com>
2024-01-01 19:28:06 +0200

    Solve 2023 day 20 part 2

--- 2023/day20/day_20_pulse_propagation.py
+++ 2023/day20/day_20_pulse_propagation.py
@@ -1,4 +1,5 @@
 import collections
+import math
 import os
 from enum import Enum
 
@@ -17,10 +18,11 @@ class Pulse(Enum):
 class Module:
     memory: bool | dict
 
-    def __init__(self, module_name: str, module_type: ModuleType, outputs: list[str]):
+    def __init__(self, module_name: str, module_type: ModuleType, outputs: list[str], monitor=False):
         self.name = module_name
         self.type = module_type
         self.outputs = outputs
+        self.monitor = monitor
 
         if self.type == ModuleType.FLIPFLOP:
             self.memory = False
@@ -35,7 +37,11 @@ class Module:
 
         elif self.type == ModuleType.CONJUNCTION:
             self.memory[sender] = pulse.value
-            return Pulse.LOW if all([i for i in self.memory.values()]) else Pulse.HIGH
+            if self.monitor:
+                # for part two we want to know if this node sends out high pulse
+                return Pulse.LOW if all([i for i in self.memory.values()]) else self.name
+            else:
+                return Pulse.LOW if all([i for i in self.memory.values()]) else Pulse.HIGH
 
         return None
 
@@ -59,12 +65,32 @@ def read_input(filename: str):
             elif prefix == '&':
                 graph[name] = Module(name, ModuleType.CONJUNCTION, outputs)
 
+    # init conj modules state
     for name, module in graph.items():
         for output in module.outputs:
             if output in graph and graph[output].type == ModuleType.CONJUNCTION:
                 graph[output].memory[name] = False
 
     return graph
+
+
+def mark_special_nodes(graph):
+
+    rx_parent = ''
+    for name, module in graph.items():
+        if 'rx' in module.outputs:
+            rx_parent = name
+            break
+
+    nodes_to_watch = []
+    for name, module in graph.items():
+        if rx_parent in module.outputs:
+            nodes_to_watch.append(name)
+
+    for name in nodes_to_watch:
+        graph[name].monitor = True
+
+    return len(nodes_to_watch)
 
 
 def part1(input_file: str) -> int:
@@ -97,7 +123,36 @@ def part1(input_file: str) -> int:
 
 
 def part2(input_file: str) -> int:
-    WORKFLOW_MAP = read_input(input_file)
+    graph = read_input(input_file)
+    num_of_special_nodes = mark_special_nodes(graph)
+    broadcaster = graph.get('broadcaster')
+    ANSWER = {}
+
+    for i in range(1, 1000000000000000):
+        q = collections.deque([('broadcaster', destination, Pulse.LOW) for destination in broadcaster.outputs])
+        while q:
+            src, dst, pulse = q.popleft()
+            # print(f'{src} -> {pulse} -> {dst}')
+
+            # skip the 'output' debug node
+            if dst not in graph:
+                continue
+
+            current_module = graph[dst]
+            out_pulse = current_module.update_state(src, pulse)
+            if out_pulse is not None:
+                if type(out_pulse) is str and out_pulse not in ANSWER:
+                    ANSWER[out_pulse] = i
+                    # print(ANSWER)
+                    if len(ANSWER) == num_of_special_nodes:
+                        return math.lcm(*ANSWER.values())
+                    out_pulse = Pulse.HIGH
+
+                for connected_module in current_module.outputs:
+                    q.append((current_module.name, connected_module, out_pulse))
+
+        if i % 100000 == 0:
+            print('#', end='')
 
 
 def test():
@@ -111,9 +166,6 @@ def test():
 
     print('Part 1 OK')
 
-    # assert part2(filename) == 167409079868000
-    # print('Part 2 OK')
-
 
 def main():
     print('\n---- MAIN ----')
@@ -122,11 +174,11 @@ def main():
     solution_part1 = part1(filename)
     print(f'Solution for Part 1: {solution_part1}')
 
-    # solution_part2 = part2(filename)
-    # print(f'Solution for Part 2: {solution_part2}\n')
+    solution_part2 = part2(filename)
+    print(f'Solution for Part 2: {solution_part2}\n')
 
     assert solution_part1 == 898557000
-    # assert solution_part2 == 127447746739409
+    assert solution_part2 == 238420328103151
 
 
 if __name__ == '__main__':
