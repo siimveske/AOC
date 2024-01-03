953a8e91f096882516405aa8d11e6899d76a8995
Siim Veske <siim.veske@gmail.com>
2024-01-03 20:14:42 +0200

    Solution to 2023 day 21 part 2

--- 2023/day21/day_21_step_counter.py
+++ 2023/day21/day_21_step_counter.py
@@ -16,14 +16,25 @@ def read_input(filename: str):
     return grid, start
 
 
-def neigbours(location: tuple[int, int], grid: list[list[str]]) -> set:
+def neighbours(location: tuple[int, int], grid: list[list[str]]) -> set:
     r, c = location
     border = len(grid)
     result = set()
     for x in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]:
         xr, xc = x
-        if (0 <= xr < border) and (0 <= xc < border) and (grid[xr][xc] == '.'):
+        if (0 <= xr < border) and (0 <= xc < border) and (grid[xr][xc] != '#'):
             result.add(x)
+    return result
+
+
+def neighbour2(location: tuple[int, int], grid: list[list[str]]) -> list:
+    r, c = location
+    border = len(grid)
+    result = []
+    for x in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]:
+        xr, xc = x
+        if grid[xr % border][xc % border] != '#':
+            result.append(x)
     return result
 
 
@@ -35,7 +46,7 @@ def part1(input_file: str, steps) -> int:
         new_locations = set()
         while locations:
             location = locations.pop()
-            n = neigbours(location, grid)
+            n = neighbours(location, grid)
             new_locations = new_locations.union(n)
         locations = new_locations
 
@@ -43,8 +54,38 @@ def part1(input_file: str, steps) -> int:
     return len(locations)
 
 
-def part2(input_file: str) -> int:
-    graph = read_input(input_file)
+def part2(input_file: str, steps) -> int:
+    grid, start = read_input(input_file)
+
+    use_precalculated = True
+    datapoints = dict([(65, 3648), (196, 32781), (327, 90972)])
+    if use_precalculated is False:
+        datapoints = [(0, 1)]
+        locations = {start}
+        for i in range(steps):
+            new_locations = []
+            while locations:
+                location = locations.pop()
+                n = neighbour2(location, grid)
+                new_locations.extend(n)
+            locations = set(new_locations)
+            datapoints.append((i + 1, len(locations)))
+        datapoints = dict(datapoints)
+
+    # Solution from: https://colab.research.google.com/github/derailed-dash/Advent-of-Code/blob/master/src/AoC_2023/Dazbo's_Advent_of_Code_2023.ipynb#scrollTo=KWExJmusNQOK
+    one, two, three = [datapoints[65], datapoints[196], datapoints[327]]
+
+    #  coefficients
+    c = one
+    b = (4 * two - 3 * one - three) // 2
+    a = two - one - b
+
+    steps = 26501365
+    grid_size = len(grid)
+    x = (steps - (grid_size // 2)) // grid_size  # number of whole tile lengths
+    result = a * x ** 2 + b * x + c
+
+    return result
 
 
 def test():
@@ -52,7 +93,6 @@ def test():
 
     filename = 'test_input.txt'
     assert part1(filename, steps=6) == 16
-
     print('Part 1 OK')
 
 
@@ -63,11 +103,11 @@ def main():
     solution_part1 = part1(filename, 64)
     print(f'Solution for Part 1: {solution_part1}')
 
-    # solution_part2 = part2(filename)
-    # print(f'Solution for Part 2: {solution_part2}\n')
+    solution_part2 = part2(filename, 327)
+    print(f'Solution for Part 2: {solution_part2}\n')
 
     assert solution_part1 == 3572
-    # assert solution_part2 == 238420328103151
+    assert solution_part2 == 594606492802848
 
 
 if __name__ == '__main__':
