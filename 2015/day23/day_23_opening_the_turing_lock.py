cfe6668e385f4ab9c887ffbe45f99a28a78e8aa6
Siim Veske <siim.veske@gmail.com>
2024-02-01 13:49:06 +0200

    Start 2015 day 23 part 1

---
+++ 2015/day23/day_23_opening_the_turing_lock.py
@@ -0,0 +1,22 @@
+def part1() -> int:
+    pass
+
+
+def part2() -> int:
+    pass
+
+
+def main():
+    print("---- MAIN ----")
+
+    solution_part1 = part1()
+    print(f"Solution for Part 1: {solution_part1}")
+    # assert solution_part1 == 953
+
+    # solution_part2 = part2()
+    # print(f"Solution for Part 2: {solution_part2}\n")
+    # assert solution_part2 == 1289
+
+
+if __name__ == "__main__":
+    main()
