036d3a0a2b1451b7e77644e6d9bb7d315f5135be
Siim Veske <siim.veske@gmail.com>
2024-02-01 20:58:14 +0200

    Solve 2015 day 23 part 2

--- 2015/day23/day_23_opening_the_turing_lock.py
+++ 2015/day23/day_23_opening_the_turing_lock.py
@@ -8,49 +8,53 @@ def read_input(filename: str) -> list:
     instructions = []
     with open(input_file_path, "r") as f:
         for line in f:
-            line = line.strip().replace(',', '')
-            parts = line.split(" ")
-            if parts[0] in ['jio', 'jie']:
+            parts = line.replace(",", "").split()
+            if parts[0] in ["jio", "jie"]:
                 parts[2] = int(parts[2])
-            if parts[0] == 'jmp':
+            if parts[0] == "jmp":
                 parts[1] = int(parts[1])
             instructions.append(parts)
 
     return instructions
 
 
-def part1(filename: str) -> int:
-    registry = {'a': 0, 'b': 0}
-    instructions = read_input(filename)
-    idx = 0
+def run(instructions: list, reg_a: int, reg_b: int) -> tuple:
+    registry = {"a": reg_a, "b": reg_b}
     program_length = len(instructions)
-
+    idx = 0
     while idx < program_length:
         instruction = instructions[idx]
-        if instruction[0] == 'hlf':
+        if instruction[0] == "hlf":
             registry[instruction[1]] //= 2
-        elif instruction[0] == 'tpl':
+        elif instruction[0] == "tpl":
             registry[instruction[1]] *= 3
-        elif instruction[0] == 'inc':
+        elif instruction[0] == "inc":
             registry[instruction[1]] += 1
-        elif instruction[0] == 'jmp':
+        elif instruction[0] == "jmp":
             idx += instruction[1]
             continue
-        elif instruction[0] == 'jie':
+        elif instruction[0] == "jie":
             if registry[instruction[1]] % 2 == 0:
                 idx += instruction[2]
                 continue
-        elif instruction[0] == 'jio':
+        elif instruction[0] == "jio":
             if registry[instruction[1]] == 1:
                 idx += instruction[2]
                 continue
         idx += 1
+    return registry["a"], registry["b"]
 
-    return registry['b']
+
+def part1(filename: str) -> int:
+    instructions = read_input(filename)
+    _, b = run(instructions, 0, 0)
+    return b
 
 
-def part2() -> int:
-    pass
+def part2(filename: str) -> int:
+    instructions = read_input(filename)
+    _, b = run(instructions, 1, 0)
+    return b
 
 
 def main():
@@ -61,9 +65,9 @@ def main():
     print(f"Solution for Part 1: {solution_part1}")
     assert solution_part1 == 170
 
-    # solution_part2 = part2()
-    # print(f"Solution for Part 2: {solution_part2}\n")
-    # assert solution_part2 == 1289
+    solution_part2 = part2(filename)
+    print(f"Solution for Part 2: {solution_part2}\n")
+    assert solution_part2 == 247
 
 
 if __name__ == "__main__":
