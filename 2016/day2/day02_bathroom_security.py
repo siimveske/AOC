3d9306ae94f0b54eee9e0e0841ffd0d419c0b2c5
Siim Veske <siim.veske@gmail.com>
2024-02-11 22:08:31 +0200

    Solve 2016 day 2 part 2

--- 2016/day2/day02_bathroom_security.py
+++ 2016/day2/day02_bathroom_security.py
@@ -9,33 +9,54 @@ def read_input(filename: str) -> list[str]:
         return f.read().splitlines()
 
 
-def part1(filename: str) -> int:
+def part1(filename: str) -> str:
     instructions = read_input(filename)
     keypad = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     directions = {"U": (-1, 0), "D": (1, 0), "L": (0, -1), "R": (0, 1)}
     code = []
     r, c = 1, 1
+    length = len(keypad)
     for line in instructions:
         for direction in line:
             dr, dc = directions[direction]
-            if 0 <= r + dr < len(keypad) and 0 <= c + dc < len(keypad[0]):
+            if (0 <= r + dr < length) and (0 <= c + dc < length):
                 r += dr
                 c += dc
         code.append(keypad[r][c])
-    return int("".join(map(str, code)))
+    return "".join(map(str, code))
 
 
-def part2(filename: str) -> int:
-    commands = read_input(filename)
+def part2(filename: str) -> str:
+    instructions = read_input(filename)
+    keypad = [
+        ["", "", 1, "", ""],
+        ["", 2, 3, 4, ""],
+        [5, 6, 7, 8, 9],
+        ["", "A", "B", "C", ""],
+        ["", "", "D", "", ""],
+    ]
+    directions = {"U": (-1, 0), "D": (1, 0), "L": (0, -1), "R": (0, 1)}
+    code = []
+    row, col = 2, 0
+    length = len(keypad)
+    for line in instructions:
+        for direction in line:
+            dr, dc = directions[direction]
+            r, c = row + dr, col + dc
+            if (0 <= r < length) and (0 <= c < length) and (keypad[r][c] != ""):
+                row, col = r, c
+        code.append(keypad[row][col])
+    return "".join(map(str, code))
 
 
 def test():
     print("---- TEST ----")
     filename = "test_input.txt"
 
-    assert part1(filename) == 1985
+    assert part1(filename) == "1985"
     print("Part 1: OK")
 
+    assert part2(filename) == "5DB3"
     print("Part 2: OK")
 
 
@@ -45,11 +66,11 @@ def main():
 
     solution_part1 = part1(filename)
     print(f"Solution for Part 1: {solution_part1}")
-    # assert solution_part1 == 307
+    assert solution_part1 == "52981"
 
-    # solution_part1 = part2(filename)
-    # print(f"Solution for Part 2: {solution_part1}")
-    # assert solution_part1 == 165
+    solution_part1 = part2(filename)
+    print(f"Solution for Part 2: {solution_part1}")
+    assert solution_part1 == "74CD2"
 
 
 if __name__ == "__main__":
