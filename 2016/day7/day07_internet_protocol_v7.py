6c1255358c2e9b3d92c3152cad57919de8f463ea
Siim Veske <siim.veske@gmail.com>
2024-02-15 00:33:38 +0200

    Solve 2016 day 7 part 2

--- 2016/day7/day07_internet_protocol_v7.py
+++ 2016/day7/day07_internet_protocol_v7.py
@@ -10,13 +10,13 @@ def read_input(filename: str) -> list[str]:
         return f.read().splitlines()
 
 
-def has_abba(s):
+def has_abba(s: str) -> bool:
     pattern = r"([a-zA-Z])([a-zA-Z])\2\1"
     match = re.search(pattern, s)
     return match is not None and match.group(1) != match.group(2)
 
 
-def supports_tls(ip_address):
+def supports_tls(ip_address: str) -> bool:
     segments = ip_address.replace("[", " ").replace("]", " ").split()
     outside_brackets = segments[::2]
     inside_brackets = segments[1::2]
@@ -27,14 +27,33 @@ def supports_tls(ip_address):
     return (in_abba is False) and (out_abba is True)
 
 
+def supports_ssl(ip_address: str) -> bool:
+    segments = ip_address.replace("[", " ").replace("]", " ").split()
+    outside_brackets = segments[::2]
+    inside_brackets = segments[1::2]
+
+    for outside in outside_brackets:
+        for i in range(len(outside) - 2):
+            a, b, c = outside[i : i + 3]
+            if a == b or a != c:
+                continue
+            bab = b + a + b
+            for inside in inside_brackets:
+                if bab in inside:
+                    return True
+    return False
+
+
 def part1(filename: str) -> int:
     addesses = read_input(filename)
     filtered_addresses = [address for address in addesses if supports_tls(address)]
     return len(filtered_addresses)
 
 
-def part2(filename: str) -> str:
-    message = read_input(filename)
+def part2(filename: str) -> int:
+    addesses = read_input(filename)
+    filtered_addresses = [address for address in addesses if supports_ssl(address)]
+    return len(filtered_addresses)
 
 
 def test():
@@ -46,8 +65,11 @@ def test():
     assert supports_tls("ioxxoj[asdfgh]zxcvbn") is True
     print("Part 1: OK")
 
-    # assert part2(filename) == "advent"
-    # print("Part 2: OK")
+    assert supports_ssl("aba[bab]xy") is True
+    assert supports_ssl("xyx[xyx]xyx") is False
+    assert supports_ssl("aaa[kek]eke") is True
+    assert supports_ssl("zazbz[bzb]cdb") is True
+    print("Part 2: OK")
 
 
 def main():
@@ -57,11 +79,10 @@ def main():
     solution_part1 = part1(filename)
     print(f"Solution for Part 1: {solution_part1}")
     assert solution_part1 == 115
-    print("Part 1: OK")
 
-    # solution_part2 = part2(filename)
-    # print(f"Solution for Part 2: {solution_part2}")
-    # assert solution_part2 == "brhailro"
+    solution_part2 = part2(filename)
+    print(f"Solution for Part 2: {solution_part2}")
+    assert solution_part2 == 231
 
 
 if __name__ == "__main__":
