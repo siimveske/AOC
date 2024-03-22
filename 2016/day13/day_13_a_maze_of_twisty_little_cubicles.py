5b1401de0addc8e6a4e95ca272e9d41650a484af
Siim Veske <siim.veske@gmail.com>
2024-03-22 18:22:40 +0200

    Solve 2016 day 13 part 2

--- 2016/day13/day_13_a_maze_of_twisty_little_cubicles.py
+++ 2016/day13/day_13_a_maze_of_twisty_little_cubicles.py
@@ -6,28 +6,14 @@ def is_open_space(location: tuple[int, int], puzzle_input: int) -> bool:
     result = x * x + 3 * x + 2 * x * y + y + y * y
     result += puzzle_input
     binary = bin(result)
-    cnt_of_ones = sum([1 for c in binary if c == "1"])
+    cnt_of_ones = binary.count("1")
     return cnt_of_ones % 2 == 0
-
-
-def print_maze(puzzle_input: int):
-    rows = 7
-    cols = 10
-    for r in range(rows):
-        for c in range(cols):
-            if is_open_space(c, r, puzzle_input):
-                print(".", end="")
-            else:
-                print("#", end="")
-        print()
 
 
 def part1(puzzle_input: int, end: tuple[int, int]) -> int:
     start = (1, 1)
     queue = deque([(start, 0)])
     visited = set()
-
-    # print_maze(puzzle_input)
 
     while queue:
         (x, y), steps = queue.popleft()
@@ -36,24 +22,58 @@ def part1(puzzle_input: int, end: tuple[int, int]) -> int:
         if (x, y) in visited:
             continue
 
+        steps += 1
+        visited.add((x, y))
+
         right = (x + 1, y)
         left = (x - 1, y)
         up = (x, y + 1)
         down = (x, y - 1)
+
         if right not in visited and is_open_space(right, puzzle_input):
-            queue.append((right, steps + 1))
+            queue.append((right, steps))
         if left not in visited and left[0] >= 0 and is_open_space(left, puzzle_input):
-            queue.append((left, steps + 1))
+            queue.append((left, steps))
         if up not in visited and is_open_space(up, puzzle_input):
-            queue.append((up, steps + 1))
+            queue.append((up, steps))
         if down not in visited and down[1] >= 0 and is_open_space(down, puzzle_input):
-            queue.append((down, steps + 1))
+            queue.append((down, steps))
 
         visited.add((x, y))
 
 
 def part2() -> int:
-    pass
+    puzzle_input: int = 1358
+    start = (1, 1)
+    queue = deque([(start, 0)])
+    visited = set()
+
+    while queue:
+        (x, y), steps = queue.popleft()
+
+        if (x, y) in visited:
+            continue
+        if steps > 50:
+            continue
+
+        steps += 1
+        visited.add((x, y))
+
+        right = (x + 1, y)
+        left = (x - 1, y)
+        up = (x, y + 1)
+        down = (x, y - 1)
+
+        if right not in visited and is_open_space(right, puzzle_input):
+            queue.append((right, steps))
+        if left not in visited and left[0] >= 0 and is_open_space(left, puzzle_input):
+            queue.append((left, steps))
+        if up not in visited and is_open_space(up, puzzle_input):
+            queue.append((up, steps))
+        if down not in visited and down[1] >= 0 and is_open_space(down, puzzle_input):
+            queue.append((down, steps))
+
+    return len(visited)
 
 
 def test():
@@ -70,9 +90,9 @@ def main():
     print(f"Solution for Part 1: {solution_part1}")
     assert solution_part1 == 96
 
-    # solution_part2 = part2(filename)
-    # print(f"Solution for Part 2: {solution_part2}")
-    # assert solution_part2 == 9227674
+    solution_part2 = part2()
+    print(f"Solution for Part 2: {solution_part2}")
+    assert solution_part2 == 141
 
 
 if __name__ == "__main__":
