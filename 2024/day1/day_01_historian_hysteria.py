30edf1d8f0b39d8b284cb708e4c5c018a84f3060
Siim Veske <siim.veske@gmail.com>
2024-12-01 23:05:01 +0200

    Solve 2024 day 1 part 2

--- 2024/day1/day_01_historian_hysteria.py
+++ 2024/day1/day_01_historian_hysteria.py
@@ -1,7 +1,7 @@
 import os
 
 
-def read_input(filename: str) -> list[list[int]]:
+def read_input(filename: str) -> list[list[int, int]]:
     script_location = os.path.dirname(os.path.realpath(__file__))
     input_file_path = os.path.join(script_location, filename)
 
@@ -16,13 +16,14 @@ def read_input(filename: str) -> list[list[int]]:
 
     list_a.sort()
     list_b.sort()
-    pairs = list(zip(list_a, list_b))
+    pairs = [list_a, list_b]
 
     return pairs
 
 
 def part1(input_file: str) -> int:
-    pairs = read_input(input_file)
+    list_a, list_b = read_input(input_file)
+    pairs = list(zip(list_a, list_b))
 
     total = 0
     for a, b in pairs:
@@ -31,7 +32,16 @@ def part1(input_file: str) -> int:
 
 
 def part2(input_file: str) -> int:
-    pass
+    list_a, list_b = read_input(input_file)
+    total = 0
+    counts = {}
+
+    for number in list_a:
+        if number not in counts:
+            counts[number] = list_b.count(number)
+        total += number * counts[number]
+    return total
+
 
 
 def test():
@@ -41,9 +51,8 @@ def test():
     assert part1(filename) == 11
     print('Part 1 OK')
 
-    # filename = 'test_input2.txt'
-    # assert part2(filename) == 281
-    # print('Part 2 OK')
+    assert part2(filename) == 31
+    print('Part 2 OK')
 
 
 def main():
@@ -53,11 +62,11 @@ def main():
     solution_part1 = part1(filename)
     print(f'Solution for Part 1: {solution_part1}')
 
-    # solution_part2 = part2(filename)
-    # print(f'Solution for Part 2: {solution_part2}\n')
+    solution_part2 = part2(filename)
+    print(f'Solution for Part 2: {solution_part2}\n')
 
-    # assert solution_part1 == 55172
-    # assert solution_part2 == 54925
+    assert solution_part1 == 1765812
+    assert solution_part2 == 20520794
 
 
 if __name__ == '__main__':
