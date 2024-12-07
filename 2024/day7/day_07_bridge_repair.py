c2f474156860aa9c9fbcf4984363b07ed1e743cd
Siim Veske <siim.veske@gmail.com>
2024-12-07 19:27:41 +0200

    Solve 2024 day 7 part 2

--- 2024/day7/day_07_bridge_repair.py
+++ 2024/day7/day_07_bridge_repair.py
@@ -16,17 +16,43 @@ def read_input(filename: str) -> list[list[int]]:
             equations.append(numbers)
     return equations
 
-OPERATIONS = [add, mul]
+
 
 def part1(input_file: str) -> int:
     equations = read_input(input_file)
     total = 0
     cache = {}
+
     for equation in equations:
         expected_result, arguments = equation[0], equation[1:]
         num_of_operations = len(arguments)-1
         if num_of_operations not in cache:
-            cache[num_of_operations] = list(itertools.product(OPERATIONS, repeat=num_of_operations))
+            cache[num_of_operations] = list(itertools.product([add, mul], repeat=num_of_operations))
+
+        for operations in cache[num_of_operations]:
+            test_result = operations[0](arguments[0], arguments[1])
+            for i in range(1, num_of_operations):
+                test_result = operations[i](test_result, arguments[i+1])
+
+            if test_result == expected_result:
+                total += expected_result
+                break
+
+    return total
+
+def join(a, b):
+    return int(f'{a}{b}')
+
+def part2(input_file: str) -> int:
+    equations = read_input(input_file)
+    total = 0
+    cache = {}
+
+    for equation in equations:
+        expected_result, arguments = equation[0], equation[1:]
+        num_of_operations = len(arguments)-1
+        if num_of_operations not in cache:
+            cache[num_of_operations] = list(itertools.product([add, mul, join], repeat=num_of_operations))
 
         for operations in cache[num_of_operations]:
             test_result = operations[0](arguments[0], arguments[1])
@@ -40,11 +66,6 @@ def part1(input_file: str) -> int:
     return total
 
 
-def part2(input_file: str) -> int:
-    grid = read_input(input_file)
-    return 0
-
-
 def test():
     print('---- TEST ----')
     filename = 'test_input.txt'
@@ -52,8 +73,8 @@ def test():
     assert part1(filename) == 3749
     print('Part 1 OK')
 
-    # assert part2(filename) == 6
-    # print('Part 2 OK')
+    assert part2(filename) == 11387
+    print('Part 2 OK')
 
 
 def main():
@@ -63,11 +84,11 @@ def main():
     solution_part1 = part1(filename)
     print(f'Solution for Part 1: {solution_part1}')
 
-    # solution_part2 = part2(filename)
-    # print(f'Solution for Part 2: {solution_part2}\n')
+    solution_part2 = part2(filename)
+    print(f'Solution for Part 2: {solution_part2}\n')
 
     assert solution_part1 == 7710205485870
-    # assert solution_part2 == 1670
+    assert solution_part2 == 20928985450275
 
 
 if __name__ == '__main__':
