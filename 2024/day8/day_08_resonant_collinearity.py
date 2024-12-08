d8681d7edbbdeb4d777e5bf78fc2ab2bb6f3c4ed
Siim Veske <siim.veske@gmail.com>
2024-12-08 13:23:09 +0200

    Solve 2024 day 8 part 1

--- 2024/day8/day_08_resonant_collinearity.py
+++ 2024/day8/day_08_resonant_collinearity.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+import itertools
 import os
 
 
@@ -5,14 +7,31 @@ def read_input(filename: str) -> list[list[str]]:
     script_dir = os.path.dirname(os.path.realpath(__file__))
     input_path = os.path.join(script_dir, filename)
 
-    grid = []
+    antennas = defaultdict(list)
+    rows, cols = 0, 0
     with open(input_path, 'r') as file:
-        for line in file:
-            grid.append([*line.strip()])
-    return grid
+        grid = file.read().splitlines()
+        rows, cols = len(grid), len(grid[0])
+        for i, row in enumerate(grid):
+            for j, col in enumerate(row):
+                if col != '.':
+                    antennas[col].append((i, j))
+    return antennas, rows, cols
 
 def part1(input_file: str) -> int:
-    pass
+    graph, rows, cols = read_input(input_file)
+    antinodes = set()
+    for points in graph.values():
+        for p1, p2 in itertools.combinations(points, 2):
+            dx = p2[0] - p1[0]
+            dy = p2[1] - p1[1]
+            antinode_1 = (p1[0]-dx, p1[1]-dy)
+            antinode_2 = (p2[0]+dx, p2[1]+dy)
+            if 0 <= antinode_1[0] < rows and 0 <= antinode_1[1] < cols:
+                antinodes.add(antinode_1)
+            if 0 <= antinode_2[0] < rows and 0 <= antinode_2[1] < cols:
+                antinodes.add(antinode_2)
+    return len(antinodes)
 
 def part2(input_file: str) -> int:
     pass
@@ -21,7 +40,7 @@ def test():
     print('---- TEST ----')
     filename = 'test_input.txt'
 
-    assert part1(filename) == 41
+    assert part1(filename) == 14
     print('Part 1 OK')
 
     # assert part2(filename) == 6
