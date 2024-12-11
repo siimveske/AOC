63d69c4297df6b1083a4d2ac0f5fa6a9dabc5918
Siim Veske <siim.veske@gmail.com>
2024-12-11 16:23:37 +0200

    Solve 2024 day 11 part 1

--- 2024/day11/day_11_plutonian_pebbles.py
+++ 2024/day11/day_11_plutonian_pebbles.py
@@ -1,46 +1,55 @@
-import os
+def blink(stones: list[int], blink_count: int) -> list[int]:
+    for _ in range(blink_count):
+        new_stones = []
+        for number in stones:
+            if number == 0:
+                new_stones.append(1)
+            elif len(str(number)) % 2 == 0:
+                str_number = str(number)
+                left = str_number[: len(str_number) // 2]
+                right = str_number[len(str_number) // 2 :]
+                new_stones.append(int(left))
+                new_stones.append(int(right))
+            else:
+                new_stones.append(number * 2024)
+        stones = new_stones
+    return stones
 
-def read_input(filename: str) -> list[list[int]]:
-    script_dir = os.path.dirname(os.path.realpath(__file__))
-    input_path = os.path.join(script_dir, filename)
 
-    grid = []
-    with open(input_path, "r") as file:
-        data = file.read().splitlines()
-        for line in data:
-            grid.append(list(map(int, line)))
+def part1(stones: list[int], blink_count:int) -> int:
+    new_stones = blink(stones, blink_count)
+    return len(new_stones)
 
-    return grid
+# def part2(input_file: str) -> int:
+#     pass
 
-
-def part1(input_file: str) -> int:
-    pass
-
-def part2(input_file: str) -> int:
-    pass
 
 def test():
     print('---- TEST ----')
 
-    filename = 'test_input.txt'
-    assert part1(filename) == 1
-    print('Part 1 OK')
+    stones = [0, 1, 10, 99, 999]
+    assert part1(stones, blink_count=1) == 7
 
+    stones = [125, 17]
+    assert part1(stones, blink_count=6) == 22
+    assert part1(stones, blink_count=25) == 55312
+
+    print("Part 1 OK")
 
     # assert part2(filename) == 3
     # print('Part 2 OK')
 
 def main():
     print('\n---- MAIN ----')
-    filename = 'input.txt'
 
-    solution_part1 = part1(filename)
+    stones = [4, 4841539, 66, 5279, 49207, 134, 609568, 0]
+    solution_part1 = part1(stones, blink_count=25)
     print(f'Solution for Part 1: {solution_part1}')
 
     # solution_part2 = part2(filename)
     # print(f'Solution for Part 2: {solution_part2}\n')
 
-    # assert solution_part1 == 782
+    assert solution_part1 == 212655
     # assert solution_part2 == 1694
 
 
