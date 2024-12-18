f06748513467b384a4349aebfa5f28f6a36fe31e
Siim Veske <siim.veske@gmail.com>
2024-12-18 14:51:15 +0200

    Solve 2024 day 14 part 2

--- 2024/day14/day_014_restroom_redoubt.py
+++ 2024/day14/day_014_restroom_redoubt.py
@@ -1,5 +1,7 @@
+import itertools
 import os
 import re
+from matplotlib import pyplot as plt
 
 
 def read_input(filename: str) -> list:
@@ -12,6 +14,41 @@ def read_input(filename: str) -> list:
             x, y, vx, vy = [int(i) for i in numbers]
             robots[idx] = (x, y, vx, vy)
     return robots
+
+def explore(location, locations, visited):
+
+    visited.add(location)
+    directions = [(-1,0), (-1,1), (0,1), (1,1), (1,0), (1,-1), (0,-1), (-1,-1)]
+
+    row, col = location
+    size = 1
+
+    for dx, dy in directions:
+        new_location = (row + dx, col + dy)
+        if new_location in locations and new_location not in visited:
+            size += explore(new_location, locations, visited)
+
+    return size
+
+
+def maximum_island(grid):
+    """Return the size of the biggest island"""
+
+    locations = set()
+    for robot in grid.values():
+        x, y, vx, vy = robot
+        locations.add((x, y))
+
+    result = 0
+    visited = set()
+
+    for location in locations:
+        if location in visited:
+            continue
+        island_size = explore(location, locations, visited)
+        result = max(result, island_size)
+
+    return result
 
 
 def part1(input_file: str, width: int, height: int) -> int:
@@ -51,8 +88,36 @@ def part1(input_file: str, width: int, height: int) -> int:
     return result
 
 
-def part2(input_file: str) -> int:
-    data = read_input(input_file)
+def part2(input_file: str, width: int, height: int):
+    grid = read_input(input_file)
+    num_of_robots = len(grid)
+    max_num_of_connected_robots = num_of_robots // 8
+
+    plt.xlim(0, width)
+    plt.ylim(0, height)
+
+    for i in itertools.count(1):
+        for idx, robot in grid.items():
+            x, y, vx, vy = robot
+            new_x = (x + vx) % width
+            new_y = (y + vy) % height
+            grid[idx] = (new_x, new_y, vx, vy)
+
+        connected = maximum_island(grid)
+
+        if connected >= max_num_of_connected_robots:
+            max_num_of_connected_robots = connected
+            print(f'Step {i}: Max connected:{max_num_of_connected_robots}')
+
+            plt.clf()
+            plt.title(f'Step {i}')
+            img = [[0] * width for _ in range(height)]
+            for robot in grid.values():
+                x, y, vx, vy = robot
+                img[y][x] = 255
+            plt.imshow(img)
+            plt.show()
+
 
 
 def test():
@@ -62,9 +127,6 @@ def test():
     assert part1(filename, width=11, height=7) == 12
     print('Part 1 OK')
 
-    # assert part2(filename) == 875318608908
-    # print('Part 2 OK')
-
 
 def main():
     print('\n---- MAIN ----')
@@ -73,11 +135,9 @@ def main():
     solution_part1 = part1(filename, width=101, height=103)
     print(f'Solution for Part 1: {solution_part1}')
 
-    # solution_part2 = part2(filename)
-    # print(f'Solution for Part 2: {solution_part2}\n')
+    part2(filename, width=101, height=103)
 
     assert solution_part1 == 230686500
-    # assert solution_part2 == 102718967795500
 
 
 if __name__ == '__main__':
