e37dfe994c6ae59f83d91d5f71ae188b79d8acc6
Siim Veske <siim.veske@gmail.com>
2024-12-18 19:53:19 +0200

    Save 2024 day 15 part 1 state

--- 2024/day15/day_015_warehouse_woes.py
+++ 2024/day15/day_015_warehouse_woes.py
@@ -1,17 +1,50 @@
+from collections import deque
 import os
 
 
 def read_input(filename: str) -> list:
     file_path = os.path.join(os.path.dirname(__file__), filename)
 
+    grid = []
+    start = None
     with open(file_path, 'r') as file:
-        return file.read().splitlines()
+        raw_map, moves = file.read().split("\n\n")
+
+        # parse map
+        _map = raw_map.split("\n")
+        for r, line in enumerate(_map):
+            grid.append(list(line))
+            if start:
+                continue
+            for c, char in enumerate(line):
+                if char == "@":
+                    start = (r, c)
+
+        # parse moves
+        moves_dict = {">": (0, 1), "<": (0, -1), "^": (-1, 0), "v": (1, 0)}
+        cmds = []
+        for move in moves:
+            if move in moves_dict:
+                cmds.append(moves_dict[move])
+
+    return grid, cmds, start
 
 
 def part1(input_file: str) -> int:
-    data = read_input(input_file)
-    result = 0
-    return result
+    grid, cmds, start = read_input(input_file)
+    queue = deque(cmds)
+
+    r, c = start
+    while queue:
+        dr, dc = queue.popleft()
+        next_r, next_c = r + dr, c + dc
+        if grid[next_r][next_c] == ".":
+            grid[next_r][next_c] = "@"
+            grid[r][c] = "."
+
+        r, c = next_r, next_c
+
+    return 0
 
 
 # def part2(input_file: str) -> int:
@@ -21,7 +54,7 @@ def test():
     print('---- TEST ----')
 
     filename = 'test_input.txt'
-    assert part1(filename) == 480
+    assert part1(filename) == 2028
     print('Part 1 OK')
 
     # assert part2(filename) == 875318608908
