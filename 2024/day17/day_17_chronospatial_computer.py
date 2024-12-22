01c2f6baae9536f5164fba557b4b875f6b41e7cc
Siim Veske <siim.veske@gmail.com>
2024-12-22 20:48:28 +0200

    Solve 2024 day 17 part 1

--- 2024/day17/day_17_chronospatial_computer.py
+++ 2024/day17/day_17_chronospatial_computer.py
@@ -14,8 +14,45 @@ def read_input(filename: str) -> tuple[list[int], list[int]]:
 
 
 def part1(input_file: str) -> int:
-    data = read_input(input_file)
-    return 0
+    registry, program = read_input(input_file)
+    reg_a, reg_b, reg_c = registry
+
+    def combo(x):
+        if x in [0, 1, 2, 3]:
+            return x
+        elif x == 4:
+            return reg_a
+        elif x == 5:
+            return reg_b
+        elif x == 6:
+            return reg_c
+        else:
+            raise ValueError(f"Invalid instruction {x}")
+
+    ip = 0
+    output = []
+    while ip < len(program):
+        opcode, arg = program[ip], program[ip + 1]
+        if opcode == 0: # adv instruction
+            reg_a = reg_a // 2**combo(arg)
+        elif opcode == 1: # bxl instruction
+            reg_b = reg_b^arg
+        elif opcode == 2: # bst instruction
+            reg_b = combo(arg)%8
+        elif opcode == 3 and reg_a != 0: # jnz instruction
+            ip = arg
+            continue
+        elif opcode == 4: # bxc instruction
+            reg_b = reg_b^reg_c
+        elif opcode == 5: # out instruction
+            output.append(combo(arg) % 8)
+        elif opcode == 6: # bdv instruction
+            reg_b = reg_a // 2**combo(arg)
+        elif opcode == 7: # cdv instruction
+            reg_c = reg_a // 2**combo(arg)
+        ip += 2
+
+    return ",".join(str(i) for i in output)
 
 
 # def part2(input_file: str) -> int:
@@ -50,7 +87,7 @@ def main():
     # solution_part2 = part2(filename)
     # print(f"Solution for Part 2: {solution_part2}\n")
 
-    # assert solution_part1 == 72400
+    assert solution_part1 == "7,1,3,7,5,1,0,3,4"
     # assert solution_part2 == 435
 
 
