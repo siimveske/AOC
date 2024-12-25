89386959cec24b803239274bb4edc8de4b5f0e30
Siim Veske <siim.veske@gmail.com>
2024-12-25 21:35:43 +0200

    Solve 2024 day 18 part 1

--- 2024/day18/day_18_ram_run.py
+++ 2024/day18/day_18_ram_run.py
@@ -13,9 +13,52 @@ def read_input(filename: str) -> tuple[list[int], list[int]]:
 
     return coordinates
 
-def part1(input_file: str) -> int:
+
+def simulate(coordinates, rows, cols, steps):
+    memory = [["."] * cols for _ in range(rows)]
+
+    for i in range(steps):
+        x, y = coordinates[i]
+        memory[y][x] = '#'
+    return memory
+
+
+def find_shortest_path(memory: list[list[str]]) -> int:
+    """
+    Find shortest path from top left grid to bottom right grid.
+    """
+    rows, cols = len(memory), len(memory[0])
+    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    seen = [[False] * cols for _ in range(rows)]
+    queue = [(0, 0, 0)]  # (x, y, steps)
+
+    while queue:
+        x, y, steps = queue.pop(0)
+
+        if x == cols - 1 and y == rows - 1:
+            return steps
+
+        if seen[y][x]:
+            continue
+
+        seen[y][x] = True
+
+        for dx, dy in directions:
+            nx, ny = x + dx, y + dy
+
+            if (0 <= nx < cols) and (0 <= ny < rows) and not seen[ny][nx] and memory[ny][nx] != "#":
+                queue.append((nx, ny, steps + 1))
+
+    return -1
+
+
+def part1(input_file: str, rows: int, cols: int, steps: int) -> int:
     coordinates = read_input(input_file)
-    return 0
+    memory = simulate(coordinates, rows, cols, steps)
+    result = find_shortest_path(memory)
+    for row in memory:
+        print("".join(row))
+    return result
 
 
 # def part2(input_file: str) -> int:
@@ -28,7 +71,7 @@ def test():
     print("---- TEST ----")
 
     filename = "test_input.txt"
-    assert part1(filename) == 22
+    assert part1(filename, rows=7, cols=7, steps=12) == 22
     print("Part 1 OK")
 
     # filename = "test_input2.txt"
@@ -40,13 +83,13 @@ def main():
     print("\n---- MAIN ----")
     filename = "input.txt"
 
-    solution_part1 = part1(filename)
+    solution_part1 = part1(filename, rows=71, cols=71, steps=1024)
     print(f"Solution for Part 1: {solution_part1}")
 
     # solution_part2 = part2(filename)
     # print(f"Solution for Part 2: {solution_part2}\n")
 
-    # assert solution_part1 == "7,1,3,7,5,1,0,3,4"
+    assert solution_part1 == 276
     # assert solution_part2 == 190384113204239
 
 
