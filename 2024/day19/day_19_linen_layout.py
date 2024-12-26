211bb68cbb24e3f3465d61797e6a1236a3e5e7a7
Siim Veske <siim.veske@gmail.com>
2024-12-26 12:49:42 +0200

    Solve 2024 day 19 part 1

--- 2024/day19/day_19_linen_layout.py
+++ 2024/day19/day_19_linen_layout.py
@@ -9,10 +9,30 @@ def read_input(filename: str) -> list[tuple[int, int]]:
         b = b.splitlines()
     return a,b
 
+def can_construct(target, word_bank, memo=None):
+    if memo is None:
+        memo = {}
+    if target in memo:
+        return memo[target]
+    if target == "":
+        return True
+
+    for word in word_bank:
+        if target.startswith(word):
+            suffix = target[len(word):]
+            if can_construct(suffix, word_bank, memo):
+                memo[target] = True
+                return True
+
+    memo[target] = False
+    return False
+
 
 def part1(input_file: str) -> int:
-    data = read_input(input_file)
-    return 0
+    a, b = read_input(input_file)
+    results = {s: can_construct(s, a) for s in b}
+    result = sum(results.values())
+    return result
 
 
 # def part2(input_file: str, rows: int, cols: int) -> str:
@@ -23,7 +43,7 @@ def test():
     print("---- TEST ----")
 
     filename = "test_input.txt"
-    assert part1(filename) == 22
+    assert part1(filename) == 6
     print("Part 1 OK")
 
     # assert part2(filename, rows=7, cols=7) == "6,1"
@@ -40,7 +60,7 @@ def main():
     # solution_part2 = part2(filename, rows=71, cols=71)
     # print(f"Solution for Part 2: {solution_part2}\n")
 
-    # assert solution_part1 == 276
+    assert solution_part1 == 342
     # assert solution_part2 == "60,37"
 
 
