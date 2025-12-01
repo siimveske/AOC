247d563a65e9e93f6847d7182f71ad880ae6f888
Siim Veske <siim.veske@gmail.com>
2025-12-01 14:10:50 +0200

    Solve 2025 day 1 part 2

--- 2025/day01/day_01_secret_entrance.py
+++ 2025/day01/day_01_secret_entrance.py
@@ -26,17 +26,19 @@ def part1(input_file: str) -> int:
     return code
 
 
-# def part2(input_file: str) -> int:
-#     first_list, second_list = read_input(input_file)
-#     total = 0
-#     count_map = {}
+def part2(input_file: str) -> int:
+    numbers = read_input(input_file)
+    position = 50
+    code = 0
 
-#     for num in first_list:
-#         if num not in count_map:
-#             count_map[num] = second_list.count(num)
-#         total += num * count_map[num]
+    for number in numbers:
+        for _ in range(abs(number)):
+            step = 1 if number > 0 else -1
+            position = (position + step) % 100
+            if position == 0:
+                code += 1
 
-#     return total
+    return code
 
 
 
@@ -47,8 +49,8 @@ def test():
     assert part1(filename) == 3
     print('Part 1 OK')
 
-    # assert part2(filename) == 31
-    # print('Part 2 OK')
+    assert part2(filename) == 6
+    print('Part 2 OK')
 
 
 def main():
@@ -58,11 +60,11 @@ def main():
     solution_part1 = part1(filename)
     print(f'Solution for Part 1: {solution_part1}')
 
-    # solution_part2 = part2(filename)
-    # print(f'Solution for Part 2: {solution_part2}\n')
+    solution_part2 = part2(filename)
+    print(f'Solution for Part 2: {solution_part2}\n')
 
     assert solution_part1 == 1031
-    # assert solution_part2 == 20520794
+    assert solution_part2 == 5831
 
 
 if __name__ == '__main__':
