cdddac9643d7a5f80c685d3ade8a121c2049afe6
Siim Veske <siim.veske@gmail.com>
2025-12-01 18:17:06 +0200

    Solve 2016 day 17 part 2

--- 2016/day17/day_17_two_steps_forward.py
+++ 2016/day17/day_17_two_steps_forward.py
@@ -2,9 +2,9 @@ import hashlib
 from collections import deque
 
 
-def find_shortest_path(code: str) -> str:
+def find_all_paths(code: str) -> list[str]:
     """
-    Find shortest path from top left grid to bottom right grid.
+    Find all paths from top left grid to bottom right grid.
     """
     rows, cols = [4, 4]
     directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
@@ -12,11 +12,13 @@ def find_shortest_path(code: str) -> str:
     open_doors = set("bcdef")
     queue = deque([(0, 0, '')])  # (x, y, path)
 
+    paths = []
     while queue:
         x, y, path = queue.popleft()
 
         if x == cols - 1 and y == rows - 1:
-            return path
+            paths.append(path)
+            continue
 
         # generate md5 hash
         seed = code + path
@@ -29,14 +31,47 @@ def find_shortest_path(code: str) -> str:
                 direction_char = direction_chars[(dx, dy)]
                 queue.append((nx, ny, path + direction_char))
 
-    return ""
+    return paths
+
+
+def find_shortest_path(paths: list[str]) -> str:
+    """
+    Find shortest path from list of paths.
+    """
+    # return min(paths, key=len)
+    return paths[0]
+
+
+def find_longest_path(paths: list[str]) -> str:
+    """
+    Find longest path from list of paths.
+    """
+    # return max(paths, key=len)
+    return paths[-1]
+
+
+def part1(input_code: str) -> str:
+    paths = find_all_paths(input_code)
+    shortest_path = find_shortest_path(paths)
+    return shortest_path
+
+
+def part2(input_code: str) -> int:
+    paths = find_all_paths(input_code)
+    longest_path = find_longest_path(paths)
+    return len(longest_path)
+
 
 def test():
     print("---- TEST ----")
 
-    assert find_shortest_path("ihgpwlah") == "DDRRRD"
-    assert find_shortest_path("kglvqrro") == "DDUDRLRRUDRD"
-    assert find_shortest_path("ulqzkmiv") == "DRURDRUDDLLDLUURRDULRLDUUDDDRR"
+    assert part1("ihgpwlah") == "DDRRRD"
+    assert part1("kglvqrro") == "DDUDRLRRUDRD"
+    assert part1("ulqzkmiv") == "DRURDRUDDLLDLUURRDULRLDUUDDDRR"
+
+    assert part2("ihgpwlah") == 370
+    assert part2("kglvqrro") == 492
+    assert part2("ulqzkmiv") == 830
 
     print("OK")
 
@@ -44,14 +79,14 @@ def test():
 def main():
     print("---- MAIN ----")
 
-    solution_part1 = find_shortest_path("vkjiggvb")
+    solution_part1 = part1("vkjiggvb")
     print(f"Solution for Part 1: {solution_part1}")
 
-    # solution_part2 = solve(initial_state="10011111011011001", disk_length=35651584)
-    # print(f"Solution for Part 2: {solution_part2}")
+    solution_part2 = part2("vkjiggvb")
+    print(f"Solution for Part 2: {solution_part2}")
 
     assert solution_part1 == "RDRRULDDDR"
-    # assert solution_part2 == "01101100001100100"
+    assert solution_part2 == 392
 
 if __name__ == "__main__":
     test()
