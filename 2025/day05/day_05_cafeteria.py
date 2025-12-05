740f0210691f326dc2ee7145ac2d0944436d08ec
Siim Veske <siim.veske@gmail.com>
2025-12-05 14:12:46 +0200

    Solve 2025 day 5 part 1

---
+++ 2025/day05/day_05_cafeteria.py
@@ -0,0 +1,80 @@
+import os
+
+
+def read_input(filename: str) -> tuple[list[tuple[int, int]], list[int]]:
+    script_location = os.path.dirname(os.path.realpath(__file__))
+    input_file_path = os.path.join(script_location, filename)
+
+    with open(input_file_path, 'r') as f:
+        data = f.read().strip().split('\n\n')
+
+        # Parse ranges
+        range_lines = data[0].splitlines()
+        ranges = []
+        for r in range_lines:
+            a, b = r.split('-')
+            ranges.append((int(a), int(b)))
+
+        # Parse IDs
+        ids = data[1].splitlines()
+        ids = [int(i) for i in ids]
+
+        database = (ranges, ids)
+
+    return database
+
+
+def part1(input_file: str) -> int:
+    ranges, ids = read_input(input_file)
+    fresh_ingredient_cnt = 0
+    for _id in ids:
+        for start, end in ranges:
+            if start <= _id <= end:
+                fresh_ingredient_cnt += 1
+                break
+
+    return fresh_ingredient_cnt
+
+
+def part2(input_file: str) -> int:
+    grid = read_input(input_file)
+    removable_count = 0
+
+    removable_rolls = get_movable_positions(grid)
+    while removable_rolls:
+        for r_idx, c_idx in removable_rolls:
+            grid[r_idx][c_idx] = '.'
+            removable_count += 1
+        removable_rolls = get_movable_positions(grid)
+
+    return removable_count
+
+
+def test():
+    print('---- TEST ----')
+    filename = 'test_input.txt'
+
+    assert part1(filename) == 3
+    print('Part 1 OK')
+
+    # assert part2(filename) == 43
+    # print('Part 2 OK')
+
+
+def main():
+    print('\n---- MAIN ----')
+    filename = 'input.txt'
+
+    solution_part1 = part1(filename)
+    print(f'Solution for Part 1: {solution_part1}')
+
+    # solution_part2 = part2(filename)
+    # print(f'Solution for Part 2: {solution_part2}\n')
+
+    assert solution_part1 == 525
+    # assert solution_part2 == 8713
+
+
+if __name__ == '__main__':
+    test()
+    main()
